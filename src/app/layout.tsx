b4965b5869c274e96afa5c8a8d5cb8a833808b95
Esteban V.M. <estebanvm90@outlook.com>
2026-04-14 15:33:08 -0300

    update root layout
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import { mainFont } from '@/fonts'
+import '@/app/globals.css'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,8 +9,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
-    <html className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`} lang='en'>
-      <body className='flex min-h-full flex-col'>{children}</body>
+    <html className={`${mainFont.variable} dark h-screen min-h-192 antialiased`} lang='es'>
+      <body>{children}</body>
     </html>
   )
 }
