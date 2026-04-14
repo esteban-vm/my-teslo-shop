d6504219c23440c529924982f8a8471cc88d857e
Esteban V.M. <estebanvm90@outlook.com>
2026-04-14 14:51:19 -0300

    add navigation
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { Search, ShoppingCart } from 'lucide-react'
+import Link from 'next/link'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui'
+
+export function Navigation() {
+  return (
+    <NavigationMenu className='fixed top-2 min-w-screen'>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href='/' />}>
+            <span className='font-bold font-geist'>Teslo</span>|&nbsp;&nbsp;Shop
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href='/' />}>
+            Hombres
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href='/' />}>
+            Mujeres
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href='/' />}>
+            Niños
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href='/' />}>
+            <Search />
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href='/' />}>
+            <ShoppingCart />
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href='/' />}>
+            Menú
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
