a85946f57921d871bea227dd02452c3971cf8f21
Esteban V.M. <estebanvm90@outlook.com>
2026-05-12 13:00:52 -0300

    update shop page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation'
 import { ProductActions } from '@/actions'
+import { ProductCard } from '@/components/common'
 
 export default async function ShopPage({ searchParams }: PageProps<'/'>) {
   let { page = '1' } = await searchParams
@@ -8,15 +9,15 @@ export default async function ShopPage({ searchParams }: PageProps<'/'>) {
   const results = await ProductActions.getProductsWithImages({ page: Number.parseInt(page, 10) })
   if (results.products.length === 0) redirect('/?page=1')
 
-  console.log({ products: results })
+  console.log({ results })
 
   return (
-    <pre>
-      {/* {JSON.stringify(
-        results.map((r) => ({ name: r.title })),
-        null,
-        1
-      )} */}
-    </pre>
+    <>
+      {Array(20)
+        .fill(null)
+        .map(() => (
+          <ProductCard key={crypto.randomUUID()} />
+        ))}
+    </>
   )
 }
