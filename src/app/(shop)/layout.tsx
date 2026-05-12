e92a1415eebfdfbba9354c0f3a487578f950eb9f
Esteban V.M. <estebanvm90@outlook.com>
2026-05-12 13:00:35 -0300

    update shop layout
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,13 +1,5 @@
-import tw from 'tailwind-styled-components'
 import { Navigation } from '@/components/navigation'
 
 export default function ShopLayout({ children }: LayoutProps<'/'>) {
-  return (
-    <>
-      <Navigation />
-      <MainContainer>{children}</MainContainer>
-    </>
-  )
+  return <Navigation> {children} </Navigation>
 }
-
-const MainContainer = tw.main`container mx-auto mt-13 border border-cyan-500`
