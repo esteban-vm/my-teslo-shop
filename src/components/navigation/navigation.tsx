b17231f7a46f86ba8370d725ff44af84c86bd085
Esteban V.M. <estebanvm90@outlook.com>
2026-05-13 15:12:05 -0300

    working on navigation
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,36 +1,21 @@
 import type { ReactNode } from 'react'
-import { Menu as MenuIcon } from 'lucide-react'
-import { Button, Drawer, Menu, Navbar } from 'rsc-daisyui'
+import { Drawer } from 'rsc-daisyui'
 import tw from 'tailwind-styled-components'
+import { Navbar } from './navbar'
+import { Sidebar } from './sidebar'
 
 export function Navigation({ children }: { children: ReactNode }) {
+  const id = crypto.randomUUID()
+
   return (
-    <Drawer className='min-h-screen rounded' end id='my-drawer-3' theme=''>
+    <Drawer className='min-h-screen' end id={id} theme=''>
       <Drawer.Content className='relative flex flex-col'>
-        <Navbar className='sticky top-0 z-10 w-full'>
-          <div className='mx-2 flex-1 px-2'>Navbar Title</div>
-          <div className='hidden flex-none lg:block'>
-            <Menu horizontal vanilla>
-              <Menu.Item>Navbar Item 1</Menu.Item>
-              <Menu.Item>Navbar Item 2</Menu.Item>
-            </Menu>
-          </div>
-          <div className='flex-none lg:hidden'>
-            <Button as='label' ghost htmlFor='my-drawer-3' shape='square'>
-              <MenuIcon aria-label='icon' className='inline-block size-6 stroke-current' role='img' />
-            </Button>
-          </div>
-        </Navbar>
+        <Navbar drawerId={id} />
         <PageContainer>{children}</PageContainer>
       </Drawer.Content>
-      <Drawer.Side className='z-10 h-full lg:hidden' drawerId='my-drawer-3'>
-        <Menu as='menu' className='min-h-full w-fit bg-base-200 p-4' vanilla>
-          <Menu.Item>Sidebar Item 1</Menu.Item>
-          <Menu.Item>Sidebar Item 2</Menu.Item>
-        </Menu>
-      </Drawer.Side>
+      <Sidebar drawerId={id} />
     </Drawer>
   )
 }
 
-const PageContainer = tw.main`container mx-auto grid grid-cols-[repeat(auto-fit,minmax(--spacing(60),1fr))] gap-4 p-4`
+const PageContainer = tw.main`container mx-auto grid grid-cols-[repeat(auto-fit,minmax(--spacing(80),1fr))] gap-4 p-4`
