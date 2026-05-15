fe40bf028f97910b74bc5917a23308ffdc0c6a2f
Esteban V.M. <estebanvm90@outlook.com>
2026-05-15 09:44:17 -0300

    create closeSidebar fn
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,13 @@
 import type { ClassValue } from 'clsx'
 import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { DRAWER_ID } from './constants'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function closeSidebar() {
+  const checkbox = document.getElementById(DRAWER_ID) as HTMLInputElement
+  checkbox.checked = false
 }
