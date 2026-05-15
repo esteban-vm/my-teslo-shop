a7db581034d213ae09146b2a7147362d370c069f
Esteban V.M. <estebanvm90@outlook.com>
2026-05-15 09:44:46 -0300

    update close button
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/navigation/close-button.tsx
+++ src/components/navigation/close-button.tsx
@@ -2,16 +2,11 @@
 
 import { X } from 'lucide-react'
 import { Button } from 'rsc-daisyui'
-import { DRAWER_ID } from '@/lib/constants'
+import { closeSidebar } from '@/lib/utils'
 
 export function CloseButton() {
-  const handleClose = () => {
-    const checkbox = document.getElementById(DRAWER_ID) as HTMLInputElement
-    checkbox.checked = false
-  }
-
   return (
-    <Button ghost onClick={handleClose} shape='square' size='xs'>
+    <Button ghost onClick={closeSidebar} shape='square' size='xs'>
       <X className='stroke-current' />
     </Button>
   )
