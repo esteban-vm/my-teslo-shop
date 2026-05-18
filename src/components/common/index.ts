9733b40e405232cdb3207d110562b4b1c2958e77
Esteban V.M. <estebanvm90@outlook.com>
2026-05-18 15:22:24 -0300

    create product grid component
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export * from './page-title'
 export * from './pagination'
 export * from './product-card'
+export * from './product-grid'
---
+++ src/components/common/product-grid.ts
@@ -0,0 +1,3 @@
+import tw from 'tailwind-styled-components'
+
+export const ProductGrid = tw.main`grid grid-cols-[repeat(auto-fit,minmax(--spacing(80),1fr))] gap-4 p-4`
