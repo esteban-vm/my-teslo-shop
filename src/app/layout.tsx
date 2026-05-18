683cce55bc10796b67ba1192b05d7dc061e8f1f8
Esteban V.M. <estebanvm90@outlook.com>
2026-05-18 13:17:57 -0300

    add font for page title
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { mainFont } from '@/fonts'
+import { mainFont, titleFont } from '@/fonts'
 import '@/app/globals.css'
 
 export const metadata: Metadata = {
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
-    <html className={`${mainFont.variable} h-screen min-h-192 bg-base-200 antialiased`} lang='es'>
+    <html className={`${mainFont.variable} ${titleFont.variable} h-screen min-h-192 bg-base-200 antialiased`} lang='es'>
       <body>{children}</body>
     </html>
   )
