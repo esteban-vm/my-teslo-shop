1ba09e0ed728a36e55007ed7b6ecbcc0f44fa25b
Esteban V.M. <estebanvm90@outlook.com>
2026-05-18 15:19:22 -0300

    update product actions
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/product.actions.ts
+++ src/actions/product.actions.ts
@@ -1,12 +1,20 @@
 'use server'
 
+import type { Gender } from '@/generated/prisma/client'
 import { prisma } from '@/lib/prisma'
 
-export async function getProductsWithImages({ page = 1, take = 12 }: { page?: number; take?: number }) {
+export interface PaginationOptions {
+  page?: number
+  take?: number
+  gender?: Gender
+}
+
+export async function getProductsWithImages({ page = 1, take = 12, gender }: PaginationOptions) {
   if (Number.isNaN(page) || page < 1) page = 1
 
   const products = await prisma.product.findMany({
     take,
+    where: { gender },
     skip: (page - 1) * take,
     include: {
       images: {
@@ -16,7 +24,7 @@ export async function getProductsWithImages({ page = 1, take = 12 }: { page?: nu
     },
   })
 
-  const totalProducts = await prisma.product.count()
+  const totalProducts = await prisma.product.count({ where: { gender } })
   const totalPages = Math.ceil(totalProducts / take)
 
   return {
