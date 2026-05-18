1fd775f89d3a8f60647637fd19b4f84d08a63dc9
Esteban V.M. <estebanvm90@outlook.com>
2026-05-18 11:54:04 -0300

    create pagination utils
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/lib/pagination.ts
@@ -0,0 +1,17 @@
+import { ELLIPSIS_CHAR } from './constants'
+
+export function generatePaginationNumbers(currentPage: number, totalPages: number) {
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1)
+  }
+
+  if (currentPage <= 3) {
+    return [1, 2, 3, ELLIPSIS_CHAR, totalPages - 1, totalPages]
+  }
+
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, ELLIPSIS_CHAR, totalPages - 2, totalPages - 1, totalPages]
+  }
+
+  return [1, ELLIPSIS_CHAR, currentPage - 1, currentPage, currentPage + 1, ELLIPSIS_CHAR, totalPages]
+}
