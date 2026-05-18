f3742981d4efefefda8a325043246145bf5865c0
Esteban V.M. <estebanvm90@outlook.com>
2026-05-18 08:54:23 -0300

    update product card
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/common/product-card.tsx
+++ src/components/common/product-card.tsx
@@ -1,42 +1,49 @@
 'use client'
 
+import type { Product } from '@/generated/prisma/client'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRef } from 'react'
 import { Card } from 'rsc-daisyui'
 import tw from 'tailwind-styled-components'
 
-export function ProductCard() {
+export interface ProductCardProps extends Product {
+  images: string[]
+}
+
+export function ProductCard({ id, title, price, images }: ProductCardProps) {
   const imageRef = useRef<HTMLImageElement>(null!)
+  const imageSrc1 = `/products/${images[0]}`
+  const imageSrc2 = `/products/${images[1]}`
 
   const handleMouseEnter = () => {
-    imageRef.current.src = '/img2.jpg'
+    imageRef.current.src = imageSrc2
   }
 
   const handleMouseLeave = () => {
-    imageRef.current.src = '/img1.jpg'
+    imageRef.current.src = imageSrc1
   }
 
   return (
-    <CardContainer border>
+    <CardContainer border id={id}>
       <Link href='/'>
         <figure className='relative aspect-square'>
           <CardImage
             $as={Image}
-            alt='Imagen de producto'
+            alt={title}
             fill
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
             ref={imageRef}
-            src='/img1.jpg'
+            src={imageSrc1}
           />
         </figure>
       </Link>
       <Card.Body className='gap-1 px-3 py-2.5'>
         <Link href='/'>
-          <Card.Title className='line-clamp-1 text-base hover:opacity-75'>Título del producto</Card.Title>
+          <Card.Title className='line-clamp-1 text-base hover:opacity-75'>{title}</Card.Title>
         </Link>
-        <p className='text-sm'>{(123.45).toLocaleString('en-US', { style: 'currency', currency: 'USD' })} </p>
+        <p className='text-sm'>{price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
       </Card.Body>
     </CardContainer>
   )
