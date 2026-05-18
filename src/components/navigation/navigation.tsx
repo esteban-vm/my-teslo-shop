4e4ce4820402cf7019032ee2b3e328b453be6716
Esteban V.M. <estebanvm90@outlook.com>
2026-05-18 10:01:12 -0300

    update navigation
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,5 +1,4 @@
 import { Drawer } from 'rsc-daisyui'
-import tw from 'tailwind-styled-components'
 import { DRAWER_ID } from '@/lib/constants'
 import { Navbar } from './navbar'
 import { Sidebar } from './sidebar'
@@ -9,11 +8,9 @@ export function Navigation({ children }: Props.WithChildren) {
     <Drawer className='min-h-screen' end id={DRAWER_ID} theme=''>
       <Drawer.Content className='relative flex flex-col'>
         <Navbar />
-        <PageContainer>{children}</PageContainer>
+        {children}
       </Drawer.Content>
       <Sidebar />
     </Drawer>
   )
 }
-
-const PageContainer = tw.main`container mx-auto grid grid-cols-[repeat(auto-fit,minmax(--spacing(80),1fr))] gap-4 p-4`
