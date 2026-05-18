57706aad70204a4a63f0f756545ee798c94825ed
Esteban V.M. <estebanvm90@outlook.com>
2026-05-18 11:55:21 -0300

    add product grid to shop page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,19 +1,26 @@
 import { redirect } from 'next/navigation'
+import tw from 'tailwind-styled-components'
 import { ProductActions } from '@/actions'
 import { ProductCard } from '@/components/common'
+import { Pagination } from '@/components/pagination'
 
 export default async function ShopPage({ searchParams }: PageProps<'/'>) {
   let { page = '1' } = await searchParams
   if (Array.isArray(page)) page = '1'
 
-  const { products } = await ProductActions.getProductsWithImages({ page: Number.parseInt(page, 10) })
+  const { products, totalPages } = await ProductActions.getProductsWithImages({ page: Number.parseInt(page, 10) })
   if (products.length === 0) redirect('/?page=1')
 
   return (
     <>
-      {products.map((product) => (
-        <ProductCard key={product.id} {...product} />
-      ))}
+      <ProductGrid>
+        {products.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </ProductGrid>
+      <Pagination totalPages={totalPages} />
     </>
   )
 }
+
+const ProductGrid = tw.main`container mx-auto grid grid-cols-[repeat(auto-fit,minmax(--spacing(80),1fr))] gap-4 p-4`
