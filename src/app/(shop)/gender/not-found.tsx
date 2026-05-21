1647a28710c1306120623bf984f82a9a4d14d80e
Esteban V.M. <estebanvm90@outlook.com>
2026-05-21 09:47:10 -0300

    create not found page for genders
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/app/(shop)/gender/not-found.tsx
@@ -0,0 +1,26 @@
+import { Frown } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from 'rsc-daisyui'
+import tw from 'tailwind-styled-components'
+
+export default function NotFoundGenderPage() {
+  return (
+    <PageContainer>
+      <div className='text-center font-montserrat text-error'>
+        <h1 className='font-bold text-5xl'>
+          404 <Frown className='inline size-12 fill-current/25 align-bottom' />
+        </h1>
+        <h2 className='font-semibold text-2xl'>Género no encontrado</h2>
+        <Button as={Link} color='warning' href='/' link>
+          Regresar al inicio
+        </Button>
+      </div>
+      <div className='relative aspect-square w-full max-w-md'>
+        <Image alt='Género no encontrado' fill src='/imgs/not-found.png' />
+      </div>
+    </PageContainer>
+  )
+}
+
+const PageContainer = tw.div`-translate-1/2 absolute top-1/2 left-1/2 flex w-full flex-col-reverse items-center justify-evenly md:flex-row md:justify-center md:gap-4`
