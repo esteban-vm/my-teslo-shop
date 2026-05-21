6000effad915280901fdacec0ff36c11c4e11341
Esteban V.M. <estebanvm90@outlook.com>
2026-05-21 14:03:48 -0300

    update global types
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,13 +1,6 @@
 declare global {
   namespace JSX {
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface IntrinsicElements extends React.JSX.IntrinsicElements {}
-  }
-
-  namespace Props {
-    interface WithChildren {
-      children: React.ReactNode
-    }
   }
 }
 
