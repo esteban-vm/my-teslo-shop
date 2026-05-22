1f5a9b1007b59e85c03848dbbbc16ae1fa2086ad
Esteban V.M. <estebanvm90@outlook.com>
2026-05-22 09:26:16 -0300

    style improvements
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
-    <html className={`${mainFont.variable} ${titleFont.variable} h-screen min-h-192 bg-base-200 antialiased`} lang='es'>
-      <body>{children}</body>
+    <html className={`${mainFont.variable} ${titleFont.variable} h-screen min-h-192 antialiased`} lang='es'>
+      <body className='h-fit bg-linear-to-t from-base-200 to-base-100 font-geist'>{children}</body>
     </html>
   )
 }
