f561166fb99e3978ff938a4314c3724fd04a739f
Esteban V.M. <estebanvm90@outlook.com>
2026-05-25 16:33:57 -0300

    update product page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ export const revalidate = 604_800 // 7 días
 
 import { notFound } from 'next/navigation'
 import { ProductActions } from '@/actions'
+import { ProductSlider } from '@/components/shop'
 import { formatProductPrice } from '@/lib/helpers'
 
 export default async function ProductPage({ params }: PageProps<'/product/[slug]'>) {
@@ -10,11 +11,11 @@ export default async function ProductPage({ params }: PageProps<'/product/[slug]
   const product = await ProductActions.getProductBySlug({ slug })
   if (!product) notFound()
 
-  const { title, description, price } = product
+  const { title, description, price, images } = product
 
   return (
-    <div className='my-3 grid gap-3 md:grid-cols-3'>
-      <div className='col-span-1 bg-accent/30 md:col-span-2'></div>
+    <div className='my-3 grid gap-3 lg:grid-cols-3'>
+      <ProductSlider images={images} />
       <div className='bg-accent/30 px-3 py-1.5'>
         <h1 className='font-montserrat font-semibold text-lg'>{title}</h1>
         <p>{formatProductPrice(price)}</p>
