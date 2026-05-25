e26eee4763f4fdf006684f588bdf1ca75013a512
Esteban V.M. <estebanvm90@outlook.com>
2026-05-25 10:20:53 -0300

    working on product page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,10 +1,20 @@
+export const revalidate = 604_800 // 7 días
+
+import { notFound } from 'next/navigation'
+import { ProductActions } from '@/actions'
+
 export default async function ProductPage({ params }: PageProps<'/product/[slug]'>) {
   const { slug } = await params
 
+  const product = await ProductActions.getProductBySlug({ slug })
+  if (!product) notFound()
+
   return (
-    <div className='border border-primary'>
-      <h1>Product Page</h1>
-      <p>{slug}</p>
+    <div className='grid gap-3 md:grid-cols-3'>
+      <div className='col-span-1 bg-accent/30 md:col-span-2'></div>
+      <div className='bg-accent/30 px-5'>
+        <h1 className='font-bold font-montserrat text-xl'>{product.title}</h1>
+      </div>
     </div>
   )
 }
