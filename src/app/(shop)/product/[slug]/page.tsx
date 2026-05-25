d26f1e75c4099c5e2c1ca754380c51446b38c287
Esteban V.M. <estebanvm90@outlook.com>
2026-05-25 11:37:32 -0300

    working on product page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ export const revalidate = 604_800 // 7 días
 
 import { notFound } from 'next/navigation'
 import { ProductActions } from '@/actions'
+import { formatProductPrice } from '@/lib/helpers'
 
 export default async function ProductPage({ params }: PageProps<'/product/[slug]'>) {
   const { slug } = await params
@@ -9,11 +10,15 @@ export default async function ProductPage({ params }: PageProps<'/product/[slug]
   const product = await ProductActions.getProductBySlug({ slug })
   if (!product) notFound()
 
+  const { title, description, price } = product
+
   return (
-    <div className='grid gap-3 md:grid-cols-3'>
+    <div className='my-3 grid gap-3 md:grid-cols-3'>
       <div className='col-span-1 bg-accent/30 md:col-span-2'></div>
-      <div className='bg-accent/30 px-5'>
-        <h1 className='font-bold font-montserrat text-xl'>{product.title}</h1>
+      <div className='bg-accent/30 px-3 py-1.5'>
+        <h1 className='font-montserrat font-semibold text-lg'>{title}</h1>
+        <p>{formatProductPrice(price)}</p>
+        <p>{description}</p>
       </div>
     </div>
   )
