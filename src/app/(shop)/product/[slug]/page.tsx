aacd6259c8e9fa77cec5a293e9115b7323f8f34d
Esteban V.M. <estebanvm90@outlook.com>
2026-05-26 13:17:40 -0300

    update product page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,10 @@
 export const revalidate = 604_800 // 7 días
 
 import { notFound } from 'next/navigation'
+import { Suspense } from 'react'
+import tw from 'tailwind-styled-components'
 import { ProductActions } from '@/actions'
-import { ProductSlider } from '@/components/shop'
+import { ProductSlider, StockCounter } from '@/components/shop'
 import { formatProductPrice } from '@/lib/helpers'
 
 export default async function ProductPage({ params }: PageProps<'/product/[slug]'>) {
@@ -14,13 +16,20 @@ export default async function ProductPage({ params }: PageProps<'/product/[slug]
   const { title, description, price, images } = product
 
   return (
-    <div className='my-3 grid gap-3 lg:grid-cols-3'>
+    <PageContainer>
       <ProductSlider images={images} />
-      <div className='bg-accent/30 px-3 py-1.5'>
-        <h1 className='font-montserrat font-semibold text-lg'>{title}</h1>
+      <div className='px-3 py-1.5'>
+        <ProductTitle>{title}</ProductTitle>
+        <Suspense fallback={<CounterSkeleton />}>
+          <StockCounter slug={slug} />
+        </Suspense>
         <p>{formatProductPrice(price)}</p>
         <p>{description}</p>
       </div>
-    </div>
+    </PageContainer>
   )
 }
+
+const PageContainer = tw.div`my-3 grid gap-3 lg:grid-cols-3`
+const ProductTitle = tw.h1`font-montserrat font-semibold text-lg`
+const CounterSkeleton = tw.div`h-6 w-full animate-pulse rounded-md bg-accent/30`
