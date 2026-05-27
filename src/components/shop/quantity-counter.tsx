2ce62396b25c2b0b45761b11b8e2396d9231b9a0
Esteban V.M. <estebanvm90@outlook.com>
2026-05-27 13:29:12 -0300

    update quantity counter
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/shop/quantity-counter.tsx
+++ src/components/shop/quantity-counter.tsx
@@ -9,7 +9,7 @@ export function QuantityCounter() {
           -
         </Join.Button>
         <Join.Input as='label' className='rounded-full outline-none' size='sm'>
-          <input className='text-center' defaultValue={0} max={5} maxLength={1} min={0} readOnly size={1} type='text' />
+          <input className='text-center' defaultValue={1} max={5} maxLength={1} min={1} readOnly size={1} type='text' />
         </Join.Input>
         <Join.Button className='rounded-full' size='sm'>
           +
