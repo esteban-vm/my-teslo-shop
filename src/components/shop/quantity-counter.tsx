fb7abd4304f7a9adb2577451d233171c98043a6d
Esteban V.M. <estebanvm90@outlook.com>
2026-05-27 13:57:33 -0300

    working on quantity counter
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/shop/quantity-counter.tsx
+++ src/components/shop/quantity-counter.tsx
@@ -1,18 +1,29 @@
+'use client'
+
 import { LucideMinus, LucidePlus } from 'lucide-react'
+import { useState } from 'react'
 import { Join } from 'rsc-daisyui'
 
 export function QuantityCounter() {
+  const [count, setCount] = useState(1)
+
+  const handleClick = (value: number) => {
+    const newCount = count + value
+    if (newCount < 1 || newCount > 5) return
+    setCount(newCount)
+  }
+
   return (
     <>
       <p className='font-semibold'>Cantidad:</p>
       <Join className='space-x-1'>
-        <Join.Button className='rounded-full' shape='square' size='sm'>
+        <Join.Button className='rounded-full' onClick={() => handleClick(-1)} shape='square' size='sm'>
           <LucideMinus className='stroke-current/70' />
         </Join.Button>
         <Join.Input as='label' className='rounded-full outline-none' color='primary' size='sm'>
-          <input className='text-center' defaultValue={1} max={5} maxLength={1} min={1} readOnly size={1} type='text' />
+          <input className='text-center' max={5} maxLength={1} min={1} readOnly size={1} type='text' value={count} />
         </Join.Input>
-        <Join.Button className='rounded-full' shape='square' size='sm'>
+        <Join.Button className='rounded-full' onClick={() => handleClick(1)} shape='square' size='sm'>
           <LucidePlus className='stroke-current/70' />
         </Join.Button>
       </Join>
