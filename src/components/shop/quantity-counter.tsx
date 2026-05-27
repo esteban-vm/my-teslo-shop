66b08da58144f0a056859e71dbdb97fed127e43c
Esteban V.M. <estebanvm90@outlook.com>
2026-05-27 13:39:50 -0300

    update quantity counter
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/shop/quantity-counter.tsx
+++ src/components/shop/quantity-counter.tsx
@@ -1,3 +1,4 @@
+import { LucideMinus, LucidePlus } from 'lucide-react'
 import { Join } from 'rsc-daisyui'
 
 export function QuantityCounter() {
@@ -5,14 +6,14 @@ export function QuantityCounter() {
     <>
       <p className='font-semibold'>Cantidad:</p>
       <Join className='space-x-1'>
-        <Join.Button className='rounded-full' size='sm'>
-          -
+        <Join.Button className='rounded-full' shape='square' size='sm'>
+          <LucideMinus className='stroke-current/70' />
         </Join.Button>
-        <Join.Input as='label' className='rounded-full outline-none' size='sm'>
+        <Join.Input as='label' className='rounded-full outline-none' color='primary' size='sm'>
           <input className='text-center' defaultValue={1} max={5} maxLength={1} min={1} readOnly size={1} type='text' />
         </Join.Input>
-        <Join.Button className='rounded-full' size='sm'>
-          +
+        <Join.Button className='rounded-full' shape='square' size='sm'>
+          <LucidePlus className='stroke-current/70' />
         </Join.Button>
       </Join>
     </>
