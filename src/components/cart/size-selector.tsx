4f9e90071f012fb063b41ebfe0e2d4c3aae5ef6d
Esteban V.M. <estebanvm90@outlook.com>
2026-05-29 17:23:06 -0300

    move cart-related components
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/cart/size-selector.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { TriangleAlert } from 'lucide-react'
+import { Alert, Join } from 'rsc-daisyui'
+import { useShoppingCartUI } from '@/hooks'
+
+export function SizeSelector() {
+  const size = useShoppingCartUI((s) => s.size)
+  const sizes = useShoppingCartUI((s) => s.sizes)
+  const posted = useShoppingCartUI((s) => s.posted)
+  const setSize = useShoppingCartUI((s) => s.setSize)
+
+  return (
+    <>
+      {posted && !size && (
+        <Alert className='w-fit gap-1 px-2 py-1 text-sm' color='error' soft>
+          <TriangleAlert className='size-4 stroke-current' />
+          <span>Tamaño requerido</span>
+        </Alert>
+      )}
+      <p className='font-semibold'>Tamaño:</p>
+      <Join className='space-x-1'>
+        {sizes.map((s) => {
+          return (
+            <Join.Button active={s === size} ghost key={s} onClick={() => setSize(s)} shape='square' size='sm'>
+              {s}
+            </Join.Button>
+          )
+        })}
+      </Join>
+    </>
+  )
+}
