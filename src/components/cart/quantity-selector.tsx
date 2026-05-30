9ea6613aecfd1dcb827f3b2385bb381891ca7d8b
Esteban V.M. <estebanvm90@outlook.com>
2026-05-30 08:17:21 -0300

    update input size
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/cart/quantity-selector.tsx
+++ src/components/cart/quantity-selector.tsx
@@ -25,7 +25,7 @@ export function QuantitySelector() {
           <LucideMinus className='stroke-current/70' />
         </Join.Button>
         <Join.Input as='label' className='rounded-full outline-none' color='primary' size='sm'>
-          <input className='text-center' maxLength={2} min={1} readOnly size={2} type='text' value={quantity} />
+          <input className='text-center' maxLength={2} min={1} readOnly size={1} type='text' value={quantity} />
         </Join.Input>
         <Join.Button className='rounded-full' onClick={increaseQuantity} shape='square' size='sm'>
           <LucidePlus className='stroke-current/70' />
