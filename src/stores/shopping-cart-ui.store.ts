2c0a58aaf2c8d5fb92d0b04613d58db210dcc6cf
Esteban V.M. <estebanvm90@outlook.com>
2026-05-30 10:50:26 -0300

    update ShoppingCartUIState
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/stores/shopping-cart-ui.store.ts
+++ src/stores/shopping-cart-ui.store.ts
@@ -4,35 +4,21 @@ import { createStore } from 'zustand'
 
 export interface ShoppingCartUIState extends ShoppingCartUIProps {
   currentSize?: Size
-  quantity: number
+  setCurrentSize: (size?: Size) => void
   isPosted: boolean
-  setCurrentSize: (size: Size) => void
-  decreaseQuantity: () => void
-  increaseQuantity: () => void
   setIsPosted: (posted: boolean) => void
 }
 
 export type ShoppingCartUIStore = ReturnType<typeof createShoppingCartUIStore>
 
 export const createShoppingCartUIStore = (initProps: ShoppingCartUIProps) => {
-  return createStore<ShoppingCartUIState>()((set, get) => {
+  return createStore<ShoppingCartUIState>()((set) => {
     return {
       ...initProps,
-      quantity: 1,
-      isPosted: false,
       setCurrentSize(size) {
         set({ currentSize: size })
       },
-      decreaseQuantity() {
-        const { quantity } = get()
-        if (quantity === 1) return
-        set({ quantity: quantity - 1 })
-      },
-      increaseQuantity() {
-        const { quantity } = get()
-        if (quantity === 5) return
-        set({ quantity: quantity + 1 })
-      },
+      isPosted: false,
       setIsPosted(posted) {
         set({ isPosted: posted })
       },
