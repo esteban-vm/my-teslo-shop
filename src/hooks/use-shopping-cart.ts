da9bc45dec4aefd28b18a728ac2d867366c131e4
Esteban V.M. <estebanvm90@outlook.com>
2026-05-31 12:32:32 -0300

    update useShoppingCart hook
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/hooks/use-shopping-cart.ts
+++ src/hooks/use-shopping-cart.ts
@@ -1,8 +1,8 @@
 import type { ShoppingCartState } from '@/stores'
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 import { createShoppingCartStore } from '@/stores'
 
 export const useShoppingCart = create<ShoppingCartState>()(
-  persist(createShoppingCartStore, { name: 'teslo-shop/shopping-cart' })
+  devtools(persist(createShoppingCartStore, { name: 'teslo-shop/shopping-cart' }))
 )
