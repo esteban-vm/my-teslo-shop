765e6d7edcd406884be0b358a310b9338b7e24f9
Esteban V.M. <estebanvm90@outlook.com>
2026-06-01 19:12:59 -0300

    update cart list
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/cart/cart-list/cart-list.tsx
+++ src/components/cart/cart-list/cart-list.tsx
@@ -2,22 +2,25 @@
 
 import Link from 'next/link'
 import { Button, List } from 'rsc-daisyui'
+import { useShoppingCart } from '@/hooks'
 import { CartItem } from './cart-item'
 
 export function CartList() {
+  const cart = useShoppingCart((s) => s.cart)
+
   return (
     <List>
       <li className='px-4 pt-2'>
         <h2 className='font-semibold text-lg'>Agregar más artículos</h2>
       </li>
       <li className='px-4'>
-        <Button as={Link} className='p-0' color='info' href='/' link size='sm'>
+        <Button as={Link} className='p-0 hover:opacity-75' color='info' href='/' link size='sm'>
           Continúa comprando
         </Button>
       </li>
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      {cart.map((product) => (
+        <CartItem key={`${product.slug}-${product.size}`} product={product} />
+      ))}
     </List>
   )
 }
