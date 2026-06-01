3ba6617526c40ba54a13fc15a5014b8ed6ba9a03
Esteban V.M. <estebanvm90@outlook.com>
2026-06-01 19:12:36 -0300

    update shopping cart store
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/stores/shopping-cart.store.ts
+++ src/stores/shopping-cart.store.ts
@@ -15,24 +15,24 @@ export interface ShoppingCartState {
   cart: ShoppingCartProduct[]
   getTotalItems: () => number
   addToCart: (product: ShoppingCartProduct) => void
+  updateQuantity: (product: ShoppingCartProduct, quantity: number) => void
+  removeFromCart: (product: ShoppingCartProduct) => void
 }
 
 export const createShoppingCartStore: StateCreator<ShoppingCartState> = (set, get) => {
   return {
     cart: [],
+
     getTotalItems() {
       const { cart } = get()
       return cart.reduce((total, item) => total + item.quantity, 0)
     },
+
     addToCart(product) {
       const { cart } = get()
 
-      const checkProduct = (item: ShoppingCartProduct) => {
-        return item.id === product.id && item.size === product.size
-      }
-
       // 1. Revisar si el producto existe en el carrito con la talla seleccionada
-      const productInCart = cart.some(checkProduct)
+      const productInCart = cart.some((item) => checkProduct(item, product))
 
       if (!productInCart) {
         set({ cart: [...cart, product] })
@@ -41,7 +41,7 @@ export const createShoppingCartStore: StateCreator<ShoppingCartState> = (set, ge
 
       // 2. Si el producto existe con la talla, incrementar la cantidad
       const updatedCart = cart.map((item) => {
-        if (checkProduct(item)) {
+        if (checkProduct(item, product)) {
           return {
             ...item,
             quantity: item.quantity + product.quantity,
@@ -53,5 +53,36 @@ export const createShoppingCartStore: StateCreator<ShoppingCartState> = (set, ge
 
       set({ cart: updatedCart })
     },
+
+    updateQuantity(product, quantity) {
+      const { cart } = get()
+
+      const updatedCart = cart.map((item) => {
+        if (checkProduct(item, product)) {
+          return {
+            ...item,
+            quantity,
+          }
+        }
+
+        return item
+      })
+
+      set({ cart: updatedCart })
+    },
+
+    removeFromCart(product) {
+      const { cart } = get()
+
+      const updatedCart = cart.filter((item) => {
+        return item.id !== product.id || item.size !== product.size
+      })
+
+      set({ cart: updatedCart })
+    },
   }
+}
+
+function checkProduct(product1: ShoppingCartProduct, product2: ShoppingCartProduct) {
+  return product1.id === product2.id && product1.size === product2.size
 }
