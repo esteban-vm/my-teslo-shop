04e6e1e269917411a6222322c97f1ead552eab37
Esteban V.M. <estebanvm90@outlook.com>
2026-06-01 13:58:54 -0300

    update cart page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link'
-import { Button, List } from 'rsc-daisyui'
-import { CartItem } from '@/components/cart'
+import { CartList } from '@/components/cart'
 import { PageTitle } from '@/components/shared'
 
 export default function CartPage() {
@@ -9,19 +7,7 @@ export default function CartPage() {
       <PageTitle title='Mi carrito' />
       <div className='my-3 grid gap-3 lg:grid-cols-3'>
         <div className='px-5 py-3 lg:col-span-2'>
-          <List>
-            <li className='px-4 pt-2'>
-              <h2 className='font-semibold text-lg'>Agregar más artículos</h2>
-            </li>
-            <li className='px-4'>
-              <Button as={Link} className='p-0' color='info' href='/' link size='sm'>
-                Continúa comprando
-              </Button>
-            </li>
-            <CartItem />
-            <CartItem />
-            <CartItem />
-          </List>
+          <CartList />
         </div>
         <div className='h-64 border border-primary'></div>
       </div>
