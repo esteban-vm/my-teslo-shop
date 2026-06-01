430904a8240865604b98911dfba43df399c10396
Esteban V.M. <estebanvm90@outlook.com>
2026-06-01 08:44:16 -0300

    update cart context
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -1,18 +1,14 @@
 'use client'
 
 import type { PropsWithChildren } from 'react'
-import type { Product } from '@/generated/prisma/client'
 import type { CartContextStore } from '@/stores'
+import type { PropsWithProduct } from '@/types'
 import { createContext, useState } from 'react'
 import { createCartContextStore } from '@/stores'
 
-export interface CartProviderBaseProps {
-  product: Product & { images: string[] }
-}
-
 export const CartContext = createContext<CartContextStore | null>(null)
 
-export function CartProvider({ children, ...props }: PropsWithChildren<CartProviderBaseProps>) {
+export function CartProvider({ children, ...props }: PropsWithChildren<PropsWithProduct>) {
   const [store] = useState(() => createCartContextStore(props))
 
   return <CartContext.Provider value={store}>{children}</CartContext.Provider>
