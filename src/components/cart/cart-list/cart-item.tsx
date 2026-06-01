9a906f55fd2959e1569cb2189a508d0691e31f57
Esteban V.M. <estebanvm90@outlook.com>
2026-06-01 19:13:13 -0300

    update cart item
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/cart/cart-list/cart-item.tsx
+++ src/components/cart/cart-list/cart-item.tsx
@@ -1,20 +1,47 @@
+import type { ShoppingCartProduct } from '@/stores'
 import { Trash2 } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Button, List } from 'rsc-daisyui'
 import { CounterInput } from '@/components/shared'
+import { useShoppingCart } from '@/hooks'
+import { formatProductPrice } from '@/lib/helpers'
 
-export function CartItem() {
+export function CartItem({ product }: { product: ShoppingCartProduct }) {
+  const { id, title, image, price, size, slug, quantity } = product
+
+  const updateQuantity = useShoppingCart((s) => s.updateQuantity)
+  const removeFromCart = useShoppingCart((s) => s.removeFromCart)
+
+  const onQuantityChange = (value: number) => {
+    const newQuantity = quantity + value
+    if (newQuantity < 1 || newQuantity > 5) return
+    updateQuantity(product, newQuantity)
+  }
+
   return (
-    <List.Row className='items-center py-2 last:pb-3'>
+    <List.Row className='items-center py-2 last:pb-3' id={id}>
       <div className='relative size-24 overflow-hidden rounded-box'>
-        <Image alt='' fill src='/image.jpg' />
+        <Image alt='' fill src={`/products/${image}`} />
       </div>
-      <List.ColGrow>
-        <h3 className='text-lg tracking-wide'>Title</h3>
-        <p>$30</p>
-        <CounterInput onDecrease={() => {}} onIncrease={() => {}} quantity={1} />
+      <List.ColGrow className='space-y-1'>
+        <Link className='font-semibold hover:opacity-75' href={`/product/${slug}`}>
+          {title} - {size}
+        </Link>
+        <p className='text-sm'>{formatProductPrice(price)}</p>
+        <CounterInput
+          onDecrease={() => onQuantityChange(-1)}
+          onIncrease={() => onQuantityChange(1)}
+          quantity={quantity}
+        />
       </List.ColGrow>
-      <Button ghost shape='square' size='sm'>
+      <Button
+        ghost
+        onClick={() => removeFromCart(product)}
+        shape='square'
+        size='sm'
+        title='Eliminar producto del carrito'
+      >
         <Trash2 className='stroke-current/75' />
       </Button>
     </List.Row>
