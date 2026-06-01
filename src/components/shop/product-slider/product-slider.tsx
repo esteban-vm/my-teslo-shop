63248cee8efa2519fe1d228c62c576eaab71e468
Esteban V.M. <estebanvm90@outlook.com>
2026-06-01 16:26:40 -0300

    update product slider
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/shop/product-slider/product-slider.tsx
+++ src/components/shop/product-slider/product-slider.tsx
@@ -4,9 +4,8 @@ import type { ImageGalleryRef } from 'react-image-gallery'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { useRef } from 'react'
 import ImageGallery from 'react-image-gallery'
-import { Button } from 'rsc-daisyui'
-import tw from 'tailwind-styled-components'
 import { cn } from '@/lib/ui'
+import { SliderButton } from './slider-button'
 
 export function ProductSlider({ images }: { images: string[] }) {
   const galleryRef = useRef<ImageGalleryRef>(null)
@@ -20,7 +19,6 @@ export function ProductSlider({ images }: { images: string[] }) {
       autoPlay
       items={images.map((image) => {
         const productUrl = `/products/${image}`
-
         return {
           original: productUrl,
           originalAlt: 'Imagen del producto',
@@ -33,29 +31,11 @@ export function ProductSlider({ images }: { images: string[] }) {
       onTouchEnd={playSlide}
       onTouchStart={pauseSlide}
       ref={galleryRef}
-      renderLeftNav={(leftNavOnClick) => {
-        return (
-          <ButtonContainer>
-            <Button onClick={leftNavOnClick} shape='circle' soft>
-              <ChevronLeft />
-            </Button>
-          </ButtonContainer>
-        )
-      }}
-      renderRightNav={(rightNavOnClick) => {
-        return (
-          <ButtonContainer className='left-full -translate-x-full'>
-            <Button onClick={rightNavOnClick} shape='circle' soft>
-              <ChevronRight />
-            </Button>
-          </ButtonContainer>
-        )
-      }}
+      renderLeftNav={(leftNavOnClick) => <SliderButton icon={<ChevronLeft />} isLeft onClick={leftNavOnClick} />}
+      renderRightNav={(rightNavOnClick) => <SliderButton icon={<ChevronRight />} onClick={rightNavOnClick} />}
       showFullscreenButton={false}
       showPlayButton={false}
       slideInterval={5000}
     />
   )
 }
-
-const ButtonContainer = tw.div`absolute top-1/2 z-10 -translate-y-1/2`
---
+++ src/components/shop/product-slider/slider-button.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from 'react'
+import { Button } from 'rsc-daisyui'
+import { cn } from '@/lib/ui'
+
+export interface SliderButtonProps {
+  icon: ReactNode
+  isLeft?: boolean
+  onClick: () => void
+}
+
+export function SliderButton({ isLeft, icon, onClick }: SliderButtonProps) {
+  return (
+    <div className={cn('absolute top-1/2 z-10 -translate-y-1/2', isLeft && 'left-full -translate-x-full')}>
+      <Button onClick={onClick} shape='circle' soft>
+        {icon}
+      </Button>
+    </div>
+  )
+}
