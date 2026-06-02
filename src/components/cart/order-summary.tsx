d5000e5951c229e0b6da53b2b7366a1bddaa52b9
Esteban V.M. <estebanvm90@outlook.com>
2026-06-02 17:35:36 -0300

    update order summary
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/cart/order-summary.tsx
+++ src/components/cart/order-summary.tsx
@@ -1,15 +1,15 @@
 'use client'
 
-import { Skeleton } from 'rsc-daisyui'
 import { useShallow } from 'zustand/shallow'
 import { useMounted, useShoppingCart } from '@/hooks'
+import { SkeletonLoader } from '../shared'
 
 export function OrderSummary() {
   const { mounted } = useMounted(4)
   const { total, subtotal, tax, totalItems } = useShoppingCart(useShallow((s) => s.getSummaryInformation()))
 
   if (!mounted) {
-    return <Skeleton className='h-48 w-full rounded-box' />
+    return <SkeletonLoader text='Cargando resumen de orden' />
   }
 
   return (
