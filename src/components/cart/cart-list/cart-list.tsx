5dcfffc03b59e2a0bfcdadd5183f711edff5395a
Esteban V.M. <estebanvm90@outlook.com>
2026-06-02 10:23:27 -0300

    update cart list
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/cart/cart-list/cart-list.tsx
+++ src/components/cart/cart-list/cart-list.tsx
@@ -1,12 +1,20 @@
 'use client'
 
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import { Button, List } from 'rsc-daisyui'
 import { useShoppingCart } from '@/hooks'
 import { CartItem } from './cart-item'
 
 export function CartList() {
+  const [mounted, setMounted] = useState(false)
   const cart = useShoppingCart((s) => s.cart)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
 
   return (
     <List>
