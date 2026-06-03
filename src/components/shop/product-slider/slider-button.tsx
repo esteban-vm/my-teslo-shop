15dbfa172c79209a1abacd9d2b1f9411adc4910c
Esteban V.M. <estebanvm90@outlook.com>
2026-06-03 09:15:29 -0300

    update SliderButton style
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/shop/product-slider/slider-button.tsx
+++ src/components/shop/product-slider/slider-button.tsx
@@ -4,14 +4,14 @@ import { cn } from '@/lib/ui'
 
 export interface SliderButtonProps {
   icon: ReactNode
-  isLeft?: boolean
+  left?: boolean
   onClick: () => void
 }
 
-export function SliderButton({ isLeft, icon, onClick }: SliderButtonProps) {
+export function SliderButton({ left, icon, onClick }: SliderButtonProps) {
   return (
-    <div className={cn('absolute top-1/2 z-10 -translate-y-1/2', isLeft && 'left-full -translate-x-full')}>
-      <Button onClick={onClick} shape='circle' soft type='button'>
+    <div className={cn('absolute top-1/2 z-10 -translate-y-1/2', left && 'left-full -translate-x-full')}>
+      <Button className='[&_svg]:stroke-3!' color='info' glass onClick={onClick} shape='circle' type='button'>
         {icon}
       </Button>
     </div>
