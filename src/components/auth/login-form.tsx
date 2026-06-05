c39bea3cd5f2a03664dc7db51a78b5bc3d868ccd
Esteban V.M. <estebanvm90@outlook.com>
2026-06-05 12:21:38 -0300

    update styles for auth forms
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -9,25 +9,25 @@ export function LoginForm() {
     <form data-auth>
       <Fieldset>
         <Fieldset.Legend>Iniciar sesión</Fieldset.Legend>
-        <Button className='border-black bg-black text-white' size='sm' type='button'>
+        <Button size='sm' type='button'>
           <GithubIcon />
           Ingresar con GitHub
         </Button>
-        <Button className='border-base-300 bg-white text-black' size='sm' type='button'>
+        <Button size='sm' type='button'>
           <GoogleIcon />
           Ingresar con Google
         </Button>
-        <Divider className='m-0 text-xs'>O</Divider>
+        <Divider>O</Divider>
         <Fieldset.Label htmlFor='email'>Correo electrónico:</Fieldset.Label>
-        <Input className='w-full' id='email' placeholder='correo@ejemplo.com' size='sm' type='email' />
+        <Input id='email' placeholder='correo@ejemplo.com' size='sm' type='email' />
         <Fieldset.Label htmlFor='password'>Contraseña:</Fieldset.Label>
-        <Input className='w-full' id='password' placeholder='************' size='sm' type='password' />
-        <Button className='mt-2' color='primary' size='sm' type='submit'>
+        <Input id='password' placeholder='************' size='sm' type='password' />
+        <Button color='primary' size='sm' type='submit'>
           Iniciar sesión
         </Button>
-        <Divider className='m-0 text-xs'>O</Divider>
+        <Divider>O</Divider>
         <Button as={Link} color='secondary' href='/auth/new-account' size='sm'>
-          Crear cuenta nueva
+          Crear cuenta
         </Button>
       </Fieldset>
     </form>
--- src/components/auth/new-account-form.tsx
+++ src/components/auth/new-account-form.tsx
@@ -10,27 +10,27 @@ export function NewAccountForm() {
     <form data-auth>
       <Fieldset>
         <Fieldset.Legend>Crear cuenta</Fieldset.Legend>
-        <Button className='border-black bg-black text-white' size='sm' type='button'>
+        <Button size='sm' type='button'>
           <GithubIcon />
           Registrarse con GitHub
         </Button>
-        <Button className='border-base-300 bg-white text-black' size='sm' type='button'>
+        <Button size='sm' type='button'>
           <GoogleIcon />
           Registrarse con Google
         </Button>
-        <Divider className='m-0 text-xs'>O</Divider>
+        <Divider>O</Divider>
         <Fieldset.Label htmlFor='name'>Nombre completo:</Fieldset.Label>
-        <Input className='w-full' id='name' placeholder={ELLIPSIS_CHAR} size='sm' type='text' />
+        <Input id='name' placeholder={ELLIPSIS_CHAR} size='sm' type='text' />
         <Fieldset.Label htmlFor='email'>Correo electrónico:</Fieldset.Label>
-        <Input className='w-full' id='email' placeholder='correo@ejemplo.com' size='sm' type='email' />
+        <Input id='email' placeholder='correo@ejemplo.com' size='sm' type='email' />
         <Fieldset.Label htmlFor='password'>Contraseña:</Fieldset.Label>
-        <Input className='w-full' id='password' placeholder='************' size='sm' type='password' />
+        <Input id='password' placeholder='************' size='sm' type='password' />
         <Fieldset.Label htmlFor='repeat-password'>Repetir contraseña:</Fieldset.Label>
-        <Input className='w-full' id='repeat-password' placeholder='************' size='sm' type='password' />
-        <Button className='mt-2' color='primary' size='sm' type='submit'>
+        <Input id='repeat-password' placeholder='************' size='sm' type='password' />
+        <Button color='primary' size='sm' type='submit'>
           Crear cuenta
         </Button>
-        <Divider className='m-0 text-xs'>O</Divider>
+        <Divider>O</Divider>
         <Button as={Link} color='secondary' href='/auth/login' size='sm'>
           Iniciar sesión
         </Button>
