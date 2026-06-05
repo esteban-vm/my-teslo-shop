37381c691fb7b21528c2dbf6b67ee0065fdf8c38
Esteban V.M. <estebanvm90@outlook.com>
2026-06-05 15:56:38 -0300

    update authConfig
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -1,8 +1,12 @@
 import type { NextAuthConfig } from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import GitHubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
+import { prisma } from '@/lib/prisma'
 
 export const authConfig: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
+
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
