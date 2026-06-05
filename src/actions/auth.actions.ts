cb52c9a70cde798e91e25a5be2de276fec11fc3a
Esteban V.M. <estebanvm90@outlook.com>
2026-06-05 13:40:56 -0300

    create signInWithGoogle action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -7,3 +7,7 @@ import { actionClient } from '@/lib/safe-action'
 export const signInWithGitHub = actionClient.action(async () => {
   await signIn('github', { redirectTo: '/' satisfies Route })
 })
+
+export const signInWithGoogle = actionClient.action(async () => {
+  await signIn('google', { redirectTo: '/' satisfies Route })
+})
