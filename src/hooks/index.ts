2ee5c70b8e1f4fffe0fd79370f3e7310d77e8c2c
Esteban V.M. <estebanvm90@outlook.com>
2026-06-08 16:54:12 -0300

    cleanup
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
-export * from './use-cart-context'
 export * from './use-cart-ui'
 export * from './use-mounted'
 export * from './use-shopping-cart'
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,2 @@
-export * from './cart-context.store'
 export * from './cart-ui.store'
 export * from './shopping-cart.store'
