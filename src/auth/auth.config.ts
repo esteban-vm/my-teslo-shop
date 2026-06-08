cc6fa4c15997e125b09e7d4bbfb7e4dea091c20e
Esteban V.M. <estebanvm90@outlook.com>
2026-06-08 11:56:55 -0300

    update auth config
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -5,6 +5,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import { prisma } from '@/lib/prisma'
 
 export const authConfig: NextAuthConfig = {
+  trustHost: true,
   adapter: PrismaAdapter(prisma),
 
   providers: [
@@ -16,6 +17,25 @@ export const authConfig: NextAuthConfig = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
+
+  session: {
+    strategy: 'jwt',
+    maxAge: 604_800, // 7 días
+  },
+
+  callbacks: {
+    jwt({ token }) {
+      // console.log({ token })
+      return token
+    },
+
+    session({ session, token }) {
+      console.log({ token })
+      // console.log({ session })
+      return session
+    },
+  },
 }
