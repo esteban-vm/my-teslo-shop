473a4d9c5e004ae515566da0e65bdd0a376ea9b9
Esteban V.M. <estebanvm90@outlook.com>
2026-06-09 16:25:46 -0300

    update auth layout
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,9 @@
 import tw from 'tailwind-styled-components'
+import { verifyNoUser } from '@/lib/auth'
 
-export default function AuthLayout({ children }: LayoutProps<'/auth'>) {
+export default async function AuthLayout({ children }: LayoutProps<'/auth'>) {
+  await verifyNoUser()
+
   return <PageContainer>{children}</PageContainer>
 }
 
