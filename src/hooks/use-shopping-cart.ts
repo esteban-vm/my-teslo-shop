d0cbc445ce25a1482dad18360a3f795d77be39d1
Esteban V.M. <estebanvm90@outlook.com>
2026-06-09 09:54:10 -0300

    update use shopping cart
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/hooks/use-shopping-cart.ts
+++ src/hooks/use-shopping-cart.ts
@@ -1,8 +1,8 @@
-import type { ShoppingCartState } from '@/stores'
+import type { ShoppingCartStore } from '@/stores'
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 import { createShoppingCartStore } from '@/stores'
 
-export const useShoppingCart = create<ShoppingCartState>()(
+export const useShoppingCart = create<ShoppingCartStore>()(
   devtools(persist(createShoppingCartStore, { name: 'teslo-shop/shopping-cart' }))
 )
