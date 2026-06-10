98464c7c6087ac1bc269db1736caea0a336acea4
Esteban V.M. <estebanvm90@outlook.com>
2026-06-10 11:42:08 -0300

    create cart link
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/navigation/navbar/cart-link.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { ShoppingCart } from 'lucide-react'
+import Link from 'next/link'
+import { Button, Indicator, Skeleton } from 'rsc-daisyui'
+import { useMounted, useShoppingCart } from '@/hooks'
+
+export function CartLink() {
+  const { mounted } = useMounted(2)
+  const totalItems = useShoppingCart((s) => s.getTotalItems())
+
+  if (!mounted) {
+    return <Skeleton className='h-7 w-8 rounded-box' />
+  }
+
+  return (
+    <Indicator>
+      <Indicator.Badge className='rounded-full!' ghost size='xs'>
+        {totalItems}
+      </Indicator.Badge>
+      <Button as={Link} ghost href={totalItems === 0 ? '/cart/empty' : '/cart'} shape='square' size='sm'>
+        <ShoppingCart />
+      </Button>
+    </Indicator>
+  )
+}
