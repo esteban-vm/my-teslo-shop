a409f1959656a76637a0fad2ea6729a95fc96c3a
Esteban V.M. <estebanvm90@outlook.com>
2026-06-10 11:49:47 -0300

    create close button
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/navigation/sidebar/close-button.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { X } from 'lucide-react'
+import { Button } from 'rsc-daisyui'
+import { closeSidebar } from '@/lib/ui'
+
+export function CloseButton() {
+  return (
+    <Button ghost onClick={closeSidebar} shape='square' size='xs'>
+      <X className='stroke-current' />
+    </Button>
+  )
+}
