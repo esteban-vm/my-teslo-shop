ac6f9765be2f8423245163bdabd46cc683831a55
Esteban V.M. <estebanvm90@outlook.com>
2026-06-10 12:19:03 -0300

    create userExists fn
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,11 @@
 import { redirect } from 'next/navigation'
 import { auth } from '@/auth'
 
+export async function userExists() {
+  const session = await auth()
+  return !!session?.user
+}
+
 export async function verifyNoUser() {
   const session = await auth()
   if (session?.user) redirect('/')
