d993ece926afa4cd2d6120d9fc6e73f7b1b952f3
Esteban V.M. <estebanvm90@outlook.com>
2026-06-10 12:19:17 -0300

    fix LogoutButton
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/navigation/sidebar/logout-button.tsx
+++ src/components/navigation/sidebar/logout-button.tsx
@@ -3,10 +3,16 @@
 import { LogOut } from 'lucide-react'
 import { Menu } from 'rsc-daisyui'
 import { AuthActions } from '@/actions'
+import { closeSidebar } from '@/lib/ui'
 
 export function LogoutButton() {
+  const onLogout = () => {
+    closeSidebar()
+    AuthActions.signOut()
+  }
+
   return (
-    <Menu.Item as='button' onClick={() => AuthActions.signOut()} type='button'>
+    <Menu.Item as='button' onClick={onLogout} type='button'>
       <LogOut />
       Salir
     </Menu.Item>
