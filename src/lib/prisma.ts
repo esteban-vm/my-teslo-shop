8b24b266f6472455cfe9bae801d6d5ddd5813ec9
Esteban V.M. <estebanvm90@outlook.com>
2026-06-11 10:49:35 -0300

    update prisma config
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,37 @@
+import type { Prisma } from '@/prisma/generated/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@/prisma/generated/client'
+
+const isNotProduction = process.env.NODE_ENV !== 'production'
+
+const omitConfig = {
+  user: {
+    active: true,
+    password: true,
+    createdAt: true,
+    updatedAt: true,
+  },
+} as const satisfies Prisma.GlobalOmitConfig
 
 const pgAdapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
-const isNotProduction = process.env.NODE_ENV !== 'production'
-
-export const prisma = new PrismaClient({
+const prisma = new PrismaClient({
+  omit: omitConfig,
   adapter: pgAdapter,
   errorFormat: 'pretty',
   log: isNotProduction ? ['query'] : undefined,
 })
 
-const globalForPrisma = global as typeof global & { prisma: typeof prisma }
+const globalForPrisma = global as typeof global & {
+  prisma: typeof prisma
+}
 
 if (isNotProduction) {
   globalForPrisma.prisma = prisma
 }
+
+export type UserOmittedFields = keyof (typeof omitConfig)['user']
+
+export { prisma }
