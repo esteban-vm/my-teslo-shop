721996c40917211ba10d6f13dad5caa60fa5b29e
Esteban V.M. <estebanvm90@outlook.com>
2026-06-11 10:51:37 -0300

    add login action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -2,7 +2,9 @@
 
 import type { Route } from 'next'
 import { signIn, signOut } from '@/auth'
+import { sleepExecution } from '@/lib/helpers'
 import { actionClient } from '@/lib/safe-action'
+import { AuthSchemas } from '@/schemas'
 
 export const loginWithGitHub = actionClient.action(async () => {
   await signIn('github', { redirectTo: '/' satisfies Route })
@@ -10,6 +12,12 @@ export const loginWithGitHub = actionClient.action(async () => {
 
 export const loginWithGoogle = actionClient.action(async () => {
   await signIn('google', { redirectTo: '/' satisfies Route })
+})
+
+export const login = actionClient.inputSchema(AuthSchemas.LoginSchema).action(async ({ parsedInput }) => {
+  await sleepExecution(5)
+  const { email, password } = parsedInput
+  await signIn('credentials', { email, password, redirectTo: '/' satisfies Route })
 })
 
 export const logout = actionClient.action(async () => {
