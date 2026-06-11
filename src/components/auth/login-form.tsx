7fe50b2331454b8f8da8e8631962f27bf1dede91
Esteban V.M. <estebanvm90@outlook.com>
2026-06-11 10:52:14 -0300

    update LoginForm
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -3,11 +3,14 @@
 import Link from 'next/link'
 import { Button, Divider, Fieldset, Input } from 'rsc-daisyui'
 import { AuthActions } from '@/actions'
+import { useLoginForm } from '@/hooks'
 import { GithubIcon, GoogleIcon } from '../shared'
 
 export function LoginForm() {
+  const { handleSubmitWithAction } = useLoginForm()
+
   return (
-    <form data-auth>
+    <form data-auth noValidate onSubmit={handleSubmitWithAction}>
       <Fieldset>
         <Fieldset.Legend>Iniciar sesión</Fieldset.Legend>
         <Button onClick={() => AuthActions.loginWithGitHub()} size='sm' type='button'>
