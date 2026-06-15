e2ef7162e0cf087b1a19b2779434ad9d7d73b512
Esteban V.M. <estebanvm90@outlook.com>
2026-06-15 17:01:33 -0300

    update createUser action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import type { Route } from 'next'
+import { hash } from 'bcryptjs'
 import { signIn, signOut } from '@/auth'
 import { CredentialsSigninError } from '@/lib/errors'
 import { sleepExecution } from '@/lib/helpers'
@@ -8,34 +9,46 @@ import { prisma } from '@/lib/prisma'
 import { actionClient } from '@/lib/safe-action'
 import { AuthSchemas } from '@/schemas'
 
+const homePage: Route = '/'
+
 export const loginWithGitHub = actionClient.action(async () => {
-  await signIn('github', { redirectTo: '/' satisfies Route })
+  await signIn('github', { redirectTo: homePage })
 })
 
 export const loginWithGoogle = actionClient.action(async () => {
-  await signIn('google', { redirectTo: '/' satisfies Route })
+  await signIn('google', { redirectTo: homePage })
 })
 
 export const login = actionClient.inputSchema(AuthSchemas.LoginSchema).action(async ({ parsedInput }) => {
   await sleepExecution(5)
   const { email, password } = parsedInput
-  await signIn('credentials', { email, password, redirectTo: '/' satisfies Route })
+  await signIn('credentials', { email, password, redirectTo: homePage })
 })
 
 export const logout = actionClient.action(async () => {
-  await signOut({ redirectTo: '/' satisfies Route })
+  await sleepExecution(1)
+  await signOut({ redirectTo: homePage })
 })
 
 export const createUser = actionClient.inputSchema(AuthSchemas.NewUserSchema).action(async ({ parsedInput }) => {
-  await sleepExecution(5)
+  await sleepExecution(3)
   const { email, name, password } = parsedInput
-
   const savedUser = await prisma.user.findUnique({ where: { email, active: true } })
 
   if (savedUser) {
     throw new CredentialsSigninError('Correo electrónico en uso')
   }
 
-  const createdUser = await prisma.user.create({ data: { email, name, password } })
-  return createdUser
+  const passwordHash = await hash(password, 10)
+
+  const createdUser = await prisma.user.create({
+    data: {
+      name,
+      email: email.toLowerCase(),
+      password: passwordHash,
+    },
+    select: { email: true },
+  })
+
+  await signIn('credentials', { email: createdUser.email, password, redirectTo: homePage })
 })
