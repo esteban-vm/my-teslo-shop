e2aa19103760b3ba0970d2b1f84cb6ec2e6a4931
Esteban V.M. <estebanvm90@outlook.com>
2026-06-16 09:19:43 -0300

    update action client
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,16 +1,22 @@
-import { createSafeActionClient, DEFAULT_SERVER_ERROR_MESSAGE } from 'next-safe-action'
-import { CredentialsSigninError } from './errors'
+import { CredentialsSignin } from 'next-auth'
+import { createSafeActionClient } from 'next-safe-action'
+import { Prisma } from '@/prisma/generated/client'
 
 export const actionClient = createSafeActionClient({
   handleServerError(error) {
-    if (error instanceof CredentialsSigninError) {
-      return error.formattedMessage
+    if (error instanceof CredentialsSignin) {
+      const { message } = error
+      return message.substring(0, message.indexOf('. Read more'))
     }
 
-    if (process.env.NODE_ENV === 'development') {
-      return error.message
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      const { code, meta } = error
+
+      if (code === 'P2002' && meta?.modelName === 'User') {
+        return 'Correo electrónico en uso'
+      }
     }
 
-    return DEFAULT_SERVER_ERROR_MESSAGE
+    return 'Ha ocurrido un error'
   },
 })
