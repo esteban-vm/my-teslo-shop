9c3f945625e1eeab84ab18f129dc11807be6f6bf
Esteban V.M. <estebanvm90@outlook.com>
2026-06-17 11:46:52 -0300

    create checkout layout
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/app/(shop)/checkout/layout.tsx
@@ -0,0 +1,9 @@
+import { redirect } from 'next/navigation'
+import { auth } from '@/auth'
+
+export default async function CheckoutLayout({ children }: LayoutProps<'/checkout'>) {
+  const session = await auth()
+  if (!session?.user) redirect('/auth/login')
+
+  return <>{children}</>
+}
