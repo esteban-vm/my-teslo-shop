dd4ba1f735267a238aaeff84f733778bee92cd3f
Esteban V.M. <estebanvm90@outlook.com>
2026-06-17 16:50:09 -0300

    add theme-related constants
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
+import type { ThemeTypeWithDefault } from 'rsc-daisyui'
 import type { Gender } from '@/prisma/generated/client'
 
 // UI
@@ -6,10 +7,19 @@ export const DRAWER_ID = 'f16a4b28-58c6-41f1-9204-77a4ca50b77b'
 // Pagination
 export const ELLIPSIS_CHAR = '…'
 
-// Genders
+// Gender Map
 export const genderMap: Record<Gender, string> = {
   men: 'hombres',
   women: 'mujeres',
   kids: 'niños',
   unisex: 'todos',
+}
+
+// App Themes
+export const appThemes = ['wireframe', 'night'] as const satisfies ThemeTypeWithDefault[]
+
+// Theme map
+export const themeMap: Record<'light' | 'dark', (typeof appThemes)[number]> = {
+  light: 'wireframe',
+  dark: 'night',
 }
