518887fb74a8ae3ac77f439be4faac225ad8d05f
Esteban V.M. <estebanvm90@outlook.com>
2026-06-17 10:09:05 -0300

    working on address page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,10 +1,18 @@
+import type { Metadata } from 'next'
 import { PageTitle } from '@/components/shared'
+
+export const metadata: Metadata = {
+  title: 'Dirección de entrega',
+}
 
 export default function AddressPage() {
   return (
     <>
       <PageTitle subtitle='Dirección de entrega' title='Dirección' />
-      <div></div>
+      <div className='my-3 flex flex-col gap-3 lg:flex-row'>
+        <div className='h-32 w-full bg-accent px-5'></div>
+        <div className='h-32 w-full bg-accent px-5'></div>
+      </div>
     </>
   )
 }
