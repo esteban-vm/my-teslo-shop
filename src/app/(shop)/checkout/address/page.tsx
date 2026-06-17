ca1a6d2180f44118f14deeed01c5b6dc2875b256
Esteban V.M. <estebanvm90@outlook.com>
2026-06-17 08:57:51 -0300

    create address page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/app/(shop)/checkout/address/page.tsx
@@ -0,0 +1,10 @@
+import { PageTitle } from '@/components/shared'
+
+export default function AddressPage() {
+  return (
+    <>
+      <PageTitle subtitle='Dirección de entrega' title='Dirección' />
+      <div></div>
+    </>
+  )
+}
