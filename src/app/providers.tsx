8e2e8a7d0af9d7c46b66d0122be8655753f97d9d
Esteban V.M. <estebanvm90@outlook.com>
2026-06-17 17:08:35 -0300

    setup theme provider
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,23 @@
 
 import type { ReactNode } from 'react'
 import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from 'next-themes'
 import { ToastContainer } from 'react-toastify'
+import { appThemes, themeMap } from '@/lib/constants'
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <SessionProvider>
-      {children}
-      <ToastContainer pauseOnHover={false} position='bottom-center' theme='colored' />
-    </SessionProvider>
+    <ThemeProvider
+      disableTransitionOnChange
+      enableSystem
+      storageKey='teslo-shop/current-theme'
+      themes={appThemes}
+      value={themeMap}
+    >
+      <SessionProvider>
+        {children}
+        <ToastContainer pauseOnHover={false} position='bottom-center' theme='colored' />
+      </SessionProvider>
+    </ThemeProvider>
   )
 }
