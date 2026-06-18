a59c4a571d05a280a9ae62f8ea81cb03dd87c0af
Esteban V.M. <estebanvm90@outlook.com>
2026-06-18 15:11:54 -0300

    create product info
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/shop/product-info.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { GlobeIcon } from 'lucide-react'
+import { useEffect, useRef, useState } from 'react'
+import { Button } from 'rsc-daisyui'
+
+export function ProductInfo({ info }: { info: string }) {
+  const translator = useRef<Translator>(null)
+  const [productInfo, setProductInfo] = useState(info)
+  const [canTranslate, setCanTranslate] = useState(false)
+
+  useEffect(() => {
+    if ('Translator' in self) {
+      const init = async () => {
+        const options: TranslatorCreateCoreOptions = {
+          sourceLanguage: 'en',
+          targetLanguage: 'es',
+        }
+
+        const translatorCapabilities = await Translator.availability(options)
+
+        if (translatorCapabilities === 'available') {
+          translator.current = await Translator.create(options)
+          setCanTranslate(true)
+        }
+      }
+
+      init()
+    }
+
+    return () => translator.current?.destroy()
+  }, [])
+
+  const onTranslate = async () => {
+    if (translator.current) {
+      setCanTranslate(false)
+      const translation = await translator.current.translate(productInfo)
+      setProductInfo(translation)
+    }
+  }
+
+  return (
+    <p className='text-justify indent-8 text-sm'>
+      {productInfo}&nbsp;
+      {canTranslate && (
+        <Button onClick={onTranslate} shape='square' size='xs' soft title='Traducir'>
+          <GlobeIcon className='size-[75%]' />
+        </Button>
+      )}
+    </p>
+  )
+}
