c029f75c0dd33b52b2143c516bba766678dd7b0a
Esteban V.M. <estebanvm90@outlook.com>
2026-06-18 09:33:51 -0300

    update theme switch
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/navigation/sidebar/theme-switch.tsx
+++ src/components/navigation/sidebar/theme-switch.tsx
@@ -1,12 +1,18 @@
 'use client'
 
-import { Moon, Sun } from 'lucide-react'
+import { MoonStar, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
-import { Menu } from 'rsc-daisyui'
+import { Menu, Skeleton } from 'rsc-daisyui'
 import tw from 'tailwind-styled-components'
+import { useMounted } from '@/hooks'
 
 export function ThemeSwitch() {
+  const { mounted } = useMounted(5)
   const { resolvedTheme, setTheme } = useTheme()
+
+  if (!mounted) {
+    return <Skeleton className='mx-auto h-8 w-16 rounded-full' />
+  }
 
   const onThemeChange = () => {
     setTheme((value) => (value === 'light' ? 'dark' : 'light'))
@@ -14,13 +20,13 @@ export function ThemeSwitch() {
 
   return (
     <Menu.Item as='div' className='self-center'>
-      <Container title='Cambiar tema'>
+      <Toggle title='Cambiar tema'>
         <input checked={resolvedTheme === 'dark'} onChange={onThemeChange} type='checkbox' />
-        <Sun aria-label='sun' />
-        <Moon aria-label='moon' />
-      </Container>
+        <Sun aria-label='sun' className='fill-current' />
+        <MoonStar aria-label='moon' />
+      </Toggle>
     </Menu.Item>
   )
 }
 
-const Container = tw.label`toggle scale-[120%] text-base-content dark:rounded-full dark:before:rounded-full [&_svg]:size-full`
+const Toggle = tw.label`toggle scale-[120%] rounded-full text-base-content before:rounded-full [&_svg]:size-full`
