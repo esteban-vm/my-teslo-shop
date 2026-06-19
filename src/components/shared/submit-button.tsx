156ae4abda83a3cbcd07a914bbd1fa885507f329
Esteban V.M. <estebanvm90@outlook.com>
2026-06-19 15:50:12 -0300

    create SubmitButton
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/shared/submit-button.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import type { Control, FieldValues } from 'react-hook-form'
+import { useFormState } from 'react-hook-form'
+import { Button } from 'rsc-daisyui'
+
+export type SubmitButtonBaseProps = Omit<Parameters<typeof Button>[0], 'type'>
+
+export interface SubmitButtonProps<T extends FieldValues> extends SubmitButtonBaseProps {
+  control: Control<T>
+}
+
+export function SubmitButton<T extends FieldValues>({ control, ...rest }: SubmitButtonProps<T>) {
+  const { isValid } = useFormState({ control })
+  return <Button {...rest} color='primary' disabled={!isValid} type='submit' />
+}
