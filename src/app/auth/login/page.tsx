e02c1365c0aff462f671802cf90a80a29ef7aadd
Esteban V.M. <estebanvm90@outlook.com>
2026-06-19 16:01:02 -0300

    update LoginPage
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { LoginForm } from '@/components/auth'
+import { LoginForm } from '@/components/forms'
 
 export const metadata: Metadata = {
   title: 'Iniciar sesión',
