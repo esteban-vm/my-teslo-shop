403d7e8c55d33354eaa675a539a4c681564406aa
Esteban V.M. <estebanvm90@outlook.com>
2026-06-19 16:00:32 -0300

    update ProfilePage
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { redirect } from 'next/navigation'
 import { auth } from '@/auth'
-import { PageTitle } from '@/components/shared'
+import { PageTitle } from '@/components/pages'
 
 export const metadata: Metadata = {
   title: 'Mi perfil',
