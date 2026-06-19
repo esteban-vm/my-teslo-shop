dd980a950c2ebdd8c4fdf86e65355dac1715d8ed
Esteban V.M. <estebanvm90@outlook.com>
2026-06-19 08:51:40 -0300

    update root metadata
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,23 @@ import { Providers } from './providers'
 import '@/app/globals.css'
 
 export const metadata: Metadata = {
-  title: {
-    template: '%s - Teslo | Shop',
-    default: 'Inicio - Teslo | Shop',
-  },
-  description: 'Una tienda virtual de ropa',
+  applicationName: 'Teslo Shop',
+  title: { template: '%s - Teslo | Shop', default: 'Inicio - Teslo | Shop' },
+  description: 'Una tienda virtual de ropa creada con Create Next App',
+  keywords: ['Next.js', 'React', 'Tailwind CSS', 'TypeScript', 'E-commerce'],
+  authors: { name: 'Esteban V.M.', url: 'https://github.com/esteban-vm' },
+  creator: 'Esteban V.M.',
+  publisher: 'Esteban V.M.',
+  generator: 'Next.js',
+  referrer: 'origin-when-cross-origin',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  interactiveWidget: 'overlays-content',
 }
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
