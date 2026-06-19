54ac631ae0859933cbade7614099181a4ee38d4a
Esteban V.M. <estebanvm90@outlook.com>
2026-06-19 15:58:14 -0300

    update ProductNotFound
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/product/not-found.tsx
+++ src/app/(shop)/product/not-found.tsx
@@ -1,4 +1,4 @@
-import { PageNotFound } from '@/components/shared'
+import { PageNotFound } from '@/components/pages'
 
 export default function ProductNotFound() {
   return <PageNotFound title='Producto no encontrado' />
