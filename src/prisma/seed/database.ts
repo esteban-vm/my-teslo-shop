5079ca35c8ba81c2603337d9c42c167d0b1ddf61
Esteban V.M. <estebanvm90@outlook.com>
2026-06-23 10:39:17 -0300

    update seed
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/prisma/seed/database.ts
+++ src/prisma/seed/database.ts
@@ -1,20 +1,22 @@
 import type { CategoryName, Prisma } from '../generated/client'
 import { prisma } from '@/lib/prisma'
-import { initialProducts, initialUsers } from '../data'
+import { countries, products, users } from '../data'
 
 async function main() {
   await prisma.user.deleteMany()
+  await prisma.country.deleteMany()
   await prisma.picture.deleteMany()
   await prisma.product.deleteMany()
   await prisma.category.deleteMany()
 
-  await prisma.user.createMany({ data: initialUsers })
+  await prisma.user.createMany({ data: users })
+  await prisma.country.createMany({ data: countries })
 
   const categories = await prisma.category.createManyAndReturn({
     data: [{ name: 'Shirts' }, { name: 'Pants' }, { name: 'Hoodies' }, { name: 'Hats' }],
   })
 
-  const categoriesMap = categories.reduce(
+  const categoryMap = categories.reduce(
     (map, category) => {
       map[category.name] = category.id
       return map
@@ -22,11 +24,11 @@ async function main() {
     {} as Record<CategoryName, string>
   )
 
-  for (const initialProduct of initialProducts) {
+  for (const initialProduct of products) {
     const { images, category, ...rest } = initialProduct
 
     const product = await prisma.product.create({
-      data: { ...rest, categoryId: categoriesMap[category] },
+      data: { ...rest, categoryId: categoryMap[category] },
     })
 
     await prisma.picture.createMany({
