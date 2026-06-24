4a68b8b3cfc1e842d817599209d8f5800f3e14db
Esteban V.M. <estebanvm90@outlook.com>
2026-06-24 14:49:59 -0300

    update getCountries action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/country.actions.ts
+++ src/actions/country.actions.ts
@@ -1,9 +1,12 @@
 'use server'
 
 import type { Country } from '@/prisma/generated/client'
+import { sleepExecution } from '@/lib/helpers'
 import { prisma } from '@/lib/prisma'
+import { actionClient } from '@/lib/safe-action'
 
-export async function getCountries(): Promise<Country[]> {
+export const getCountries = actionClient.action(async (): Promise<Country[]> => {
+  await sleepExecution()
   const countries = await prisma.country.findMany({ orderBy: { name: 'asc' } })
   return countries
-}
+})
