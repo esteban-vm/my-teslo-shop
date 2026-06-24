d71f41b3330bddc0acfe52afb29366eea5132af5
Esteban V.M. <estebanvm90@outlook.com>
2026-06-24 16:20:00 -0300

    create address store
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/stores/address.store.ts
@@ -0,0 +1,34 @@
+import type { StateCreator } from 'zustand'
+import type { AddressSchemas } from '@/schemas'
+
+type Address = AddressSchemas.AddressDTO
+
+interface AddressState {
+  address: Address
+}
+
+interface AddressActions {
+  setAddress: (address: Address) => void
+}
+
+export type AddressStore = AddressState & AddressActions
+
+export const createAddressStore: StateCreator<AddressStore> = (set) => {
+  return {
+    address: {
+      firstName: '',
+      lastName: '',
+      address: '',
+      address2: '',
+      postalCode: '',
+      phone: '',
+      city: '',
+      countryId: '',
+      remember: false,
+    },
+
+    setAddress(address) {
+      set({ address })
+    },
+  }
+}
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,2 +1,3 @@
+export * from './address.store'
 export * from './cart-ui.store'
 export * from './shopping-cart.store'
