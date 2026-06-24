749ea8f4939f096922a0b418e1eb916df95a84fb
Esteban V.M. <estebanvm90@outlook.com>
2026-06-24 14:28:17 -0300

    update AddressForm
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/forms/address-form.tsx
+++ src/components/forms/address-form.tsx
@@ -1,9 +1,51 @@
 'use client'
 
 import type { Country } from '@/prisma/generated/client'
+import { Fieldset } from 'rsc-daisyui'
+import tw from 'tailwind-styled-components'
+import { useAddressForm } from '@/hooks'
+import { CountrySelector, FormField, SubmitButton } from '../shared'
 
 export function AddressForm({ countries }: { countries: Country[] }) {
-  console.log({ countries })
+  const {
+    form: { control },
+    isDisabled,
+    handleSubmitWithAction,
+  } = useAddressForm()
 
-  return <div></div>
+  return (
+    <Container noValidate onSubmit={handleSubmitWithAction}>
+      <Fieldset disabled={isDisabled}>
+        <Fieldset.Legend>Dirección de entrega</Fieldset.Legend>
+        <Row>
+          <FormField autoComplete='given-name' control={control} label='Nombres' name='firstName' />
+          <FormField autoComplete='family-name' control={control} label='Apellidos' name='lastName' />
+        </Row>
+        <Row>
+          <FormField autoComplete='street-address' control={control} label='Dirección' name='address' />
+          <FormField
+            autoComplete='street-address'
+            control={control}
+            label='Dirección 2 (opcional)'
+            name='address2'
+            required={false}
+          />
+        </Row>
+        <Row>
+          <FormField autoComplete='postal-code' control={control} label='Código postal' name='postalCode' />
+          <FormField autoComplete='address-level2' control={control} label='Ciudad' name='city' />
+        </Row>
+        <Row>
+          <CountrySelector countries={countries} />
+          <FormField autoComplete='tel' control={control} label='Teléfono' name='phone' type='tel' />
+        </Row>
+        <SubmitButton className='w-fit' control={control}>
+          Siguiente
+        </SubmitButton>
+      </Fieldset>
+    </Container>
+  )
 }
+
+const Container = tw.form`mx-auto w-full max-w-5xl px-5`
+const Row = tw.div`flex w-full flex-col lg:flex-row lg:gap-5`
