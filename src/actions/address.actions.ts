416bbdf5366b96345fd0e33f8423b25cfc5092b6
Esteban V.M. <estebanvm90@outlook.com>
2026-06-24 11:17:37 -0300

    update setAddress action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/address.actions.ts
+++ src/actions/address.actions.ts
@@ -1,5 +1,22 @@
 'use server'
 
+import { sleepExecution } from '@/lib/helpers'
+import { prisma } from '@/lib/prisma'
 import { authClient } from '@/lib/safe-action'
+import { AddressSchemas } from '@/schemas'
 
-export const setUserAddress = authClient.action(async () => {})
+export const setAddress = authClient.inputSchema(AddressSchemas.AddressDTO).action(async ({ ctx, parsedInput }) => {
+  await sleepExecution(1)
+
+  const userId = ctx.user.id
+  const { remember: _, ...rest } = parsedInput
+  const data = { userId, ...rest }
+
+  const address = await prisma.userAddress.upsert({
+    where: { userId },
+    create: data,
+    update: data,
+  })
+
+  return address
+})
