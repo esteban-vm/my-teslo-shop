2342f339ce0c7d8948d78c6da9c8b2c116c61681
Esteban V.M. <estebanvm90@outlook.com>
2026-06-25 15:40:38 -0300

    update AddressPage
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 }
 
 export default async function AddressPage() {
-  const { data: address } = await AddressActions.getUserAddress()
+  const { data: address } = await AddressActions.getBillingAddress()
 
   return (
     <>
