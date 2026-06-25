57a9b37d53a2d2b07a9cba0e6e047a152a2f38b0
Esteban V.M. <estebanvm90@outlook.com>
2026-06-25 12:35:11 -0300

    refactor
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/country.actions.ts
+++ src/actions/country.actions.ts
@@ -1,11 +1,10 @@
 'use server'
 
-import type { Country } from '@/prisma/generated/client'
 import { sleepExecution } from '@/lib/helpers'
 import { prisma } from '@/lib/prisma'
 import { actionClient } from '@/lib/safe-action'
 
-export const getCountries = actionClient.action(async (): Promise<Country[]> => {
+export const getCountries = actionClient.action(async () => {
   await sleepExecution()
   const countries = await prisma.country.findMany({ orderBy: { name: 'asc' } })
   return countries
