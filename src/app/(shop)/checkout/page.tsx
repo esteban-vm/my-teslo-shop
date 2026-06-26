9465eb866606d0f34a730f3e228a05f5c7c2e4ae
Esteban V.M. <estebanvm90@outlook.com>
2026-06-26 12:07:12 -0300

    update CheckoutPage
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,13 +1,23 @@
 import type { Metadata } from 'next'
+import { CheckoutList, PlaceOrder } from '@/components/checkout'
+import { PageTitle } from '@/components/pages'
 
 export const metadata: Metadata = {
-  title: 'Caja',
+  title: 'Verificar orden',
 }
 
 export default function CheckoutPage() {
   return (
-    <div>
-      <h1>Checkout Page</h1>
-    </div>
+    <>
+      <PageTitle title='Verificar orden' />
+      <div className='my-3 grid gap-3 px-4 lg:grid-cols-3' data-shop>
+        <div className='lg:col-span-2'>
+          <CheckoutList />
+        </div>
+        <div>
+          <PlaceOrder />
+        </div>
+      </div>
+    </>
   )
 }
