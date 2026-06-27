f575216e57c88b26cecc26f3e70bd8d929535d90
Esteban V.M. <estebanvm90@outlook.com>
2026-06-27 12:39:58 -0300

    cleanup
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/schemas/order.ts
+++ src/schemas/order.ts
@@ -1,18 +1,10 @@
 import { z } from 'zod'
 import { Size } from '@/prisma/generated/client'
 
-export const OrderDTO = z.object({
-  total: z.number(),
-  subtotal: z.number(),
-  tax: z.number(),
-  totalItems: z.int(),
-})
-
 export const OrderItemDTO = z.object({
   productId: z.cuid2(),
   quantity: z.int(),
   size: z.enum(Size),
 })
 
-export type OrderDTO = z.infer<typeof OrderDTO>
 export type OrderItemDTO = z.infer<typeof OrderItemDTO>
