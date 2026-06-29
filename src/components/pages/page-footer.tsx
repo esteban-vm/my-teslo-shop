f70bb4e1f95af0ec47306da25e23be421416b16a
Esteban V.M. <estebanvm90@outlook.com>
2026-06-29 16:35:04 -0300

    fix style
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/pages/page-footer.tsx
+++ src/components/pages/page-footer.tsx
@@ -2,7 +2,7 @@ import { Footer } from 'rsc-daisyui'
 
 export function PageFooter() {
   return (
-    <Footer center className='sm:footer-horizontal bg-base-300 p-4 text-base-content'>
+    <Footer center className='sm:footer-horizontal px-4 pt-0 pb-2 text-base-content'>
       <aside>
         <p className='font-montserrat font-semibold text-xs'>
           <span className='text-rose-500'>Teslo</span> | Shop © {new Date().getFullYear()}
