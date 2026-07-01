35d3b3ac883b0464838bdd4f3fdbabe8183d0f6a
Esteban V.M. <estebanvm90@outlook.com>
2026-07-01 12:26:40 -0300

    update PlaceOrder
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/checkout/place-order.tsx
+++ src/components/checkout/place-order.tsx
@@ -6,15 +6,14 @@ import { Button, Card, Divider, Skeleton } from 'rsc-daisyui'
 import { useShallow } from 'zustand/shallow'
 import { placeOrder } from '@/actions/order'
 import { useAddressStore, useMounted, useShoppingCart } from '@/hooks'
-import { formatProductPrice } from '@/lib/helpers'
 import { Toasts } from '@/lib/toasts'
+import { OrderSummary, ShippingAddress } from '../shared'
 
 export function PlaceOrder() {
   const { mounted } = useMounted(4)
-  const address = useAddressStore((s) => s.address)
   const cart = useShoppingCart((s) => s.cart)
   const resetCart = useShoppingCart((s) => s.resetCart)
-  const summary = useShoppingCart(useShallow((s) => s.getOrderSummary()))
+  const address = useAddressStore(useShallow((s) => s.address))
 
   const { isExecuting, execute, reset } = useAction(placeOrder, {
     onSettled() {
@@ -42,9 +41,6 @@ export function PlaceOrder() {
     return <Skeleton text>Cargando datos de orden</Skeleton>
   }
 
-  const { total, subtotal, tax, totalItems } = summary
-  const { firstName, lastName, city, countryId, phone, ...rest } = address
-
   const onPlaceOrder = () => {
     const items: OrderItemDTO[] = cart.map((item) => {
       return {
@@ -60,36 +56,9 @@ export function PlaceOrder() {
   return (
     <Card>
       <Card.Body className='px-4 pt-2 pb-3'>
-        <Card.Title>Dirección de entrega</Card.Title>
-        <div>
-          <p>
-            {firstName} {lastName}
-          </p>
-          {Object.values(rest).map((value) => (
-            <p key={crypto.randomUUID()}>{value}</p>
-          ))}
-          <p>
-            {city}, {countryId}
-          </p>
-          <p>{phone}</p>
-        </div>
+        <ShippingAddress />
         <Divider className='my-0' />
-        <Card.Title>Resumen de orden</Card.Title>
-        <div>
-          <p>
-            Nro. de artículos:
-            <span className='float-end'>{totalItems}</span>
-          </p>
-          <p>
-            Subtotal:<span className='float-end'>{formatProductPrice(subtotal)}</span>
-          </p>
-          <p>
-            Impuestos (15%):<span className='float-end'>{formatProductPrice(tax)}</span>
-          </p>
-          <p className='mt-1 font-semibold text-base'>
-            Total:<span className='float-end'>{formatProductPrice(total)}</span>
-          </p>
-        </div>
+        <OrderSummary />
         <Button className='mx-auto' disabled={isExecuting} onClick={onPlaceOrder} size='sm' wide>
           Colocar orden
         </Button>
