83c7855c2346920b64586f9bc9d0d609249900c9
Esteban V.M. <estebanvm90@outlook.com>
2026-07-02 13:14:16 -0300

    update OrderPage
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from 'next'
+import type { CartProduct } from '@/types'
+import { notFound } from 'next/navigation'
+import { Card, Divider } from 'rsc-daisyui'
+import { getOrderById } from '@/actions/order'
 import { OrderList } from '@/components/orders'
 import { PageTitle } from '@/components/pages'
+import { OrderSummary, ShippingAddress } from '@/components/shared'
 
 export const metadata: Metadata = {
   title: 'Detalle de orden',
@@ -9,14 +14,37 @@ export const metadata: Metadata = {
 export default async function OrderPage({ params }: PageProps<'/orders/[id]'>) {
   const { id } = await params
 
+  const { data: order } = await getOrderById({ id })
+  if (!order) notFound()
+
+  const { items, shippingAddress: savedAddress, ...savedSummary } = order
+
+  const products: CartProduct[] = items.map(({ product, ...rest }) => {
+    return {
+      ...rest,
+      id: product.id,
+      title: product.title,
+      slug: product.slug,
+      image: product.images.map((image) => image.url)[0],
+    }
+  })
+
   return (
     <>
-      <PageTitle subtitle={id} title='Orden #' />
+      <PageTitle subtitle={id} title='Orden ID:' />
       <section data-shop>
         <div className='lg:col-span-2'>
-          <OrderList />
+          <OrderList products={products} />
         </div>
-        <div></div>
+        <div>
+          <Card>
+            <Card.Body>
+              {savedAddress && <ShippingAddress savedAddress={savedAddress} />}
+              <Divider />
+              <OrderSummary savedSummary={savedSummary} />
+            </Card.Body>
+          </Card>
+        </div>
       </section>
     </>
   )
