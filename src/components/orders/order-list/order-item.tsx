4143ffd9201176e7dd71b593ad7c99a0f26fed8e
Esteban V.M. <estebanvm90@outlook.com>
2026-07-02 10:28:46 -0300

    update OrderItem
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/orders/order-list/order-item.tsx
+++ src/components/orders/order-list/order-item.tsx
@@ -4,10 +4,10 @@ import { ItemImage } from '@/components/shared'
 import { formatProductPrice } from '@/lib/helpers'
 
 export function OrderItem({ product }: { product: CartProduct }) {
-  const { title, image, price, size, quantity } = product
+  const { id, title, image, price, size, quantity } = product
 
   return (
-    <List.Row className='product-item'>
+    <List.Row className='product-item' id={id}>
       <ItemImage image={image} title={title} />
       <List.ColGrow className='font-semibold'>
         <p>
