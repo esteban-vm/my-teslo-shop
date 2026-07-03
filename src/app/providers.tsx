408650da26b31b0eb627bb7a650b4197d3211f2e
Esteban V.M. <estebanvm90@outlook.com>
2026-07-03 14:06:31 -0300

    update Providers
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import type { ReactNode } from 'react'
-import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 import { ToastContainer } from 'react-toastify'
 import { themeMap, themes } from '@/lib/constants'
@@ -15,10 +14,8 @@ export function Providers({ children }: { children: ReactNode }) {
       themes={themes}
       value={themeMap}
     >
-      <SessionProvider>
-        {children}
-        <ToastContainer pauseOnHover={false} position='bottom-center' theme='colored' />
-      </SessionProvider>
+      {children}
+      <ToastContainer pauseOnHover={false} position='bottom-center' theme='colored' />
     </ThemeProvider>
   )
 }
