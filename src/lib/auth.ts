2e45d4cf473c4cfdbc9f97f3bfe2c53bacfe2cec
Esteban V.M. <estebanvm90@outlook.com>
2026-07-03 14:33:31 -0300

    update auth
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,20 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
   user: {
     modelName: 'users',
   },
