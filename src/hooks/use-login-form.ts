fef732be6865888ef77ee287f21919a39fabf5e5
Esteban V.M. <estebanvm90@outlook.com>
2026-07-04 11:17:50 -0300

    update useLoginForm hook
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/hooks/use-login-form.ts
+++ src/hooks/use-login-form.ts
@@ -1,14 +1,14 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useHookFormAction } from '@next-safe-action/adapter-react-hook-form/hooks'
 import { useState } from 'react'
-import { loginWithCredentials } from '@/actions/auth'
+import { login } from '@/actions/auth'
 import { Toasts } from '@/lib/toasts'
 import { Login } from '@/schemas/auth'
 
 export function useLoginForm() {
   const [isServerError, setIsServerError] = useState(false)
 
-  const methods = useHookFormAction(loginWithCredentials, zodResolver(Login), {
+  const methods = useHookFormAction(login, zodResolver(Login), {
     formProps: {
       mode: 'onChange',
       defaultValues: {
