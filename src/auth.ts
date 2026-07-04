1bc9174f9ea45992eec0e8bb8223f119784c2bd4
Esteban V.M. <estebanvm90@outlook.com>
2026-07-04 11:19:25 -0300

    update auth config
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,18 @@
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
+import { nextCookies } from 'better-auth/next-js'
 import { prisma } from './lib/prisma'
 
 export const auth = betterAuth({
+  plugins: [nextCookies()],
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
   emailAndPassword: {
     enabled: true,
-    requireEmailVerification: true,
+    autoSignIn: true,
+    minPasswordLength: 8,
+    // requireEmailVerification: true,
   },
   emailVerification: {
     sendOnSignIn: true,
