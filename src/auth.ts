c21c5edd56b0b78cf71d80977458fdfa10b30ee4
Esteban V.M. <estebanvm90@outlook.com>
2026-07-06 12:22:54 -0300

    update auth config
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,12 @@
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import { nextCookies } from 'better-auth/next-js'
+import { sendEmail } from '@/lib/emails'
 import { prisma } from '@/lib/prisma'
 
 export const auth = betterAuth({
   plugins: [nextCookies()],
   baseURL: process.env.BETTER_AUTH_URL,
-  trustedOrigins: [process.env.BETTER_AUTH_URL!],
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
@@ -14,14 +14,29 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
     minPasswordLength: 8,
-    // requireEmailVerification: true,
+    requireEmailVerification: true,
   },
   emailVerification: {
     sendOnSignIn: true,
     sendOnSignUp: true,
     autoSignInAfterVerification: true,
-    async sendVerificationEmail({ user, url, token }) {
-      console.log('Verification email:', { user, url, token })
+    async sendVerificationEmail({ user, url }) {
+      await sendEmail({
+        to: {
+          name: user.name,
+          address: user.email,
+        },
+        priority: 'high',
+        subject: 'Verificación de correo electrónico',
+        html: `
+          <p>Verifica tu correo electrónico clicando el siguiente enlace:</p>
+          <p>
+            <a href="${url}" target="_blank" rel="noopener noreferrer">
+              <b>Verificar correo electrónico</b>
+            </a>
+          </p>
+        `,
+      })
     },
   },
   socialProviders: {
