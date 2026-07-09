a63342e2062417aa7bc92aa1f91307cd06dd6153
Esteban V.M. <estebanvm90@outlook.com>
2026-07-09 10:27:17 -0300

    update shared schemas
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/schemas/shared.ts
+++ src/schemas/shared.ts
@@ -2,6 +2,7 @@ import z from 'zod'
 import { ValidationErrorMap } from '@/lib/constants'
 import { Validations } from '@/lib/validations'
 
+export const WithID = z.object({ id: z.string() })
 export const notEmpty = z.string().trim().refine(Validations.notEmpty, ValidationErrorMap.notEmpty)
 
 export const passwordParams = {
