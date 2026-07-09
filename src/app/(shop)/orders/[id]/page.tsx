92791f1d18a6635a0518f5add1fcd5d05c6a4913
Esteban V.M. <estebanvm90@outlook.com>
2026-07-09 15:18:38 -0300

    update OrderPage
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from 'next'
 import type { CartProduct } from '@/types'
-import { Banknote, BanknoteX } from 'lucide-react'
 import { notFound } from 'next/navigation'
-import { Alert, Card, Divider } from 'rsc-daisyui'
+import { Card, Divider } from 'rsc-daisyui'
 import { getOrderById } from '@/actions/order'
-import { OrderList, PaymentButton } from '@/components/orders'
+import { OrderList, PaymentAlert, PaymentButton } from '@/components/orders'
 import { PageTitle } from '@/components/pages'
 import { AddressDetails, SummaryDetails } from '@/components/shared'
 
@@ -35,26 +34,16 @@ export default async function OrderPage({ params }: PageProps<'/orders/[id]'>) {
       <PageTitle subtitle={id} title='ID de orden:' />
       <section data-shop>
         <div className='lg:col-span-2'>
-          <Alert
-            className='not-lg:mx-auto mb-2 max-w-md gap-2 rounded-lg py-2 font-semibold'
-            color={isPaid ? 'success' : 'error'}
-          >
-            {isPaid ? <Banknote /> : <BanknoteX />}
-            <span>{isPaid ? 'Pagada' : 'No pagada'}</span>
-          </Alert>
           <OrderList products={products} />
         </div>
         <div>
           <Card>
             <Card.Body>
-              {savedAddress && <AddressDetails savedAddress={savedAddress} />}
+              <PaymentAlert isPaid={isPaid} />
+              <AddressDetails savedAddress={savedAddress} />
               <Divider />
               <SummaryDetails savedSummary={savedSummary} />
-              {/* <Alert className='gap-2 rounded-lg py-2 font-semibold' color={isPaid ? 'success' : 'error'}>
-                {isPaid ? <Banknote /> : <BanknoteX />}
-                <span>{isPaid ? 'Pagada' : 'No pagada'}</span>
-              </Alert> */}
-              <PaymentButton />
+              {!isPaid && <PaymentButton amount={savedSummary.total} orderId={order.id} />}
             </Card.Body>
           </Card>
         </div>
