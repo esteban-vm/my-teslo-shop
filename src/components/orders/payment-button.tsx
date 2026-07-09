42f316ad6695c739fa3d1e126ddc77af6b105a27
Esteban V.M. <estebanvm90@outlook.com>
2026-07-09 15:18:52 -0300

    update PaymentButton
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/orders/payment-button.tsx
+++ src/components/orders/payment-button.tsx
@@ -1,7 +1,46 @@
 'use client'
 
-import { PayPalOneTimePaymentButton } from '@paypal/react-paypal-js/sdk-v6'
+import type { PayPalButtonCreateOrder, PayPalButtonOnApprove } from '@paypal/paypal-js'
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
+import { Skeleton } from 'rsc-daisyui'
+import { checkPayment, setTransactionId } from '@/actions/order'
 
-export function PaymentButton() {
-  return <PayPalOneTimePaymentButton className='w-full' onApprove={async () => {}} orderId='' />
+export interface PaymentButtonProps {
+  orderId: string
+  amount: number
+}
+
+export function PaymentButton({ orderId, amount }: PaymentButtonProps) {
+  const [{ isPending }] = usePayPalScriptReducer()
+  const roundedAmount = Math.round(amount * 100) / 100
+
+  if (isPending) {
+    return <Skeleton text>Cargando PayPal</Skeleton>
+  }
+
+  const onCreateOrder: PayPalButtonCreateOrder = async (_, actions) => {
+    const transactionId = await actions.order.create({
+      purchase_units: [
+        {
+          invoice_id: orderId,
+          amount: {
+            currency_code: 'USD',
+            value: roundedAmount.toString(),
+          },
+        },
+      ],
+      intent: 'CAPTURE',
+    })
+
+    await setTransactionId({ orderId, transactionId })
+    return transactionId
+  }
+
+  const onApproveOrder: PayPalButtonOnApprove = async (_, actions) => {
+    const details = await actions.order?.capture()
+    if (!details?.id) return
+    await checkPayment({ id: details.id })
+  }
+
+  return <PayPalButtons className='z-10' createOrder={onCreateOrder} onApprove={onApproveOrder} />
 }
