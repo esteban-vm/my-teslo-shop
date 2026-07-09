520cf27a9aa363a3fbd7d4319eddf0413a5756d6
Esteban V.M. <estebanvm90@outlook.com>
2026-07-09 15:47:03 -0300

    update order actions
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/order.ts
+++ src/actions/order.ts
@@ -1,7 +1,10 @@
 'use server'
 
+import type { Route } from 'next'
+import { revalidatePath } from 'next/cache'
 import { ServerError } from '@/lib/errors'
 import { sleepExecution } from '@/lib/helpers'
+import { getToken, verifyPayment } from '@/lib/payments'
 import { prisma } from '@/lib/prisma'
 import { safeAuthClient } from '@/lib/safe-action'
 import { OrderDTO, TransactionDTO } from '@/schemas/order'
@@ -178,4 +181,38 @@ export const setTransactionId = safeAuthClient.inputSchema(TransactionDTO).actio
     },
     data: { transactionId },
   })
+})
+
+export const checkPayment = safeAuthClient.inputSchema(WithID).action(async ({ parsedInput }) => {
+  const payPalToken = await getToken()
+  console.log({ payPalToken })
+
+  if (!payPalToken) {
+    throw new ServerError('No se pudo encontrar token de verificación')
+  }
+
+  const statusResponse = await verifyPayment(parsedInput.id, payPalToken)
+
+  if (!statusResponse) {
+    throw new ServerError('Error al verificar el pago')
+  }
+
+  const { status, purchase_units } = statusResponse
+  console.log({ status, purchase_units })
+
+  if (status !== 'COMPLETED') {
+    throw new ServerError('Aún no se ha pagado en PayPal')
+  }
+
+  const [{ invoice_id: orderId }] = purchase_units
+
+  await prisma.order.update({
+    where: { id: orderId },
+    data: {
+      isPaid: true,
+      paidAt: new Date(),
+    },
+  })
+
+  revalidatePath(`/orders/${orderId}` satisfies Route<`/orders/${string}`>)
 })
