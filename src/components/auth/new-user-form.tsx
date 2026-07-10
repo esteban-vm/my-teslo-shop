a0c8bdcf9884eb675cd7d321e07d4f9979b6ccca
Esteban V.M. <estebanvm90@outlook.com>
2026-07-10 17:07:57 -0300

    create new user form
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/auth/new-user-form.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { AtSign, LockKeyhole, Pencil } from 'lucide-react'
+import { Divider, Fieldset } from 'rsc-daisyui'
+import { useNewUserForm } from '@/hooks'
+import { FormButton, FormField } from '../shared'
+
+export function NewUserForm() {
+  const {
+    form: { control },
+    isDisabled,
+    handleSubmitWithAction,
+  } = useNewUserForm()
+
+  return (
+    <form data-auth noValidate onSubmit={handleSubmitWithAction}>
+      <Fieldset disabled={isDisabled}>
+        <Fieldset.Legend>Registrarse</Fieldset.Legend>
+        <FormButton.Socials />
+        <Divider>O</Divider>
+        <FormField autoComplete='name' control={control} icon={<Pencil />} label='Nombre completo' name='name' />
+        <FormField
+          autoComplete='home email'
+          control={control}
+          icon={<AtSign />}
+          label='Correo electrónico'
+          name='email'
+          type='email'
+        />
+        <FormField control={control} icon={<LockKeyhole />} label='Contraseña' name='password' type='password' />
+        <FormField
+          control={control}
+          icon={<LockKeyhole />}
+          label='Repetir contraseña'
+          name='repeatPassword'
+          type='password'
+        />
+        <FormButton.Submit control={control}>Registrarse</FormButton.Submit>
+        <Divider>O</Divider>
+        <FormButton.Link to='/auth/login'>Iniciar sesión</FormButton.Link>
+      </Fieldset>
+    </form>
+  )
+}
