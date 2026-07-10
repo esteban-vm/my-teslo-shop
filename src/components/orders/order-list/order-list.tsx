5e3fcdda18e2a69e18a1d64986e369f1588cfc1f
Esteban V.M. <estebanvm90@outlook.com>
2026-07-10 17:17:29 -0300

    update order list
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/orders/order-list/order-list.tsx
+++ src/components/orders/order-list/order-list.tsx
@@ -1,13 +1,13 @@
 import type { CartProduct } from '@/types'
-import { ProductList } from '@/components/shop'
+import { ItemList } from '@/components/shared'
 import { OrderItem } from './order-item'
 
 export function OrderList({ products }: { products: CartProduct[] }) {
   return (
-    <ProductList>
+    <ItemList>
       {products.map((item) => (
         <OrderItem key={`${item.slug}-${item.size}`} product={item} />
       ))}
-    </ProductList>
+    </ItemList>
   )
 }
