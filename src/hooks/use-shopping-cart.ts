ab79369e85d9f7b63fc378acb382af7c980c178f
Esteban V.M. <estebanvm90@outlook.com>
2026-07-11 09:04:53 -0300

    cleanup
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/hooks/use-shopping-cart.ts
+++
@@ -1,8 +0,0 @@
-import type { ShoppingCartStore } from '@/stores'
-import { create } from 'zustand'
-import { devtools, persist } from 'zustand/middleware'
-import { createShoppingCartStore } from '@/stores'
-
-export const useShoppingCart = create<ShoppingCartStore>()(
-  devtools(persist(createShoppingCartStore, { name: 'teslo-shop/shopping-cart' }))
-)
