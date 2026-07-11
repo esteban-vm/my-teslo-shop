335842674f9b06dc0ab665f2f513748ce20b17c5
Esteban V.M. <estebanvm90@outlook.com>
2026-07-11 08:49:02 -0300

    update PlaceOrder
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/checkout/place-order.tsx
+++ src/components/checkout/place-order.tsx
@@ -1,41 +1,15 @@
 'use client'
 
 import type { OrderItemDTO } from '@/schemas/order'
-import { useRouter } from 'next/navigation'
-import { useAction } from 'next-safe-action/hooks'
 import { Button, Card, Divider, Skeleton } from 'rsc-daisyui'
-import { useShallow } from 'zustand/shallow'
-import { placeOrder } from '@/actions/order'
-import { useAddressStore, useMounted, useShoppingCart } from '@/hooks'
-import { Toasts } from '@/lib/toasts'
+import { useAddressStore, useMounted, usePlaceOrder, useShoppingCart } from '@/hooks'
 import { AddressDetails, SummaryDetails } from '../shared'
 
 export function PlaceOrder() {
-  const router = useRouter()
   const { mounted } = useMounted(4)
+  const { isExecuting, execute } = usePlaceOrder()
   const cart = useShoppingCart((s) => s.cart)
-  const resetCart = useShoppingCart((s) => s.resetCart)
-  const address = useAddressStore(useShallow((s) => s.address))
-  const resetAddress = useAddressStore((s) => s.resetAddress)
-
-  const { isExecuting, execute, reset } = useAction(placeOrder, {
-    onSettled() {
-      reset()
-    },
-    onExecute() {
-      Toasts.execute('Colocando orden')
-    },
-    onSuccess(args) {
-      Toasts.close()
-      router.replace(`/orders/${args.data.orderId}`)
-      resetCart()
-      resetAddress()
-    },
-    onError(args) {
-      const { serverError } = args.error
-      if (serverError) Toasts.error(serverError)
-    },
-  })
+  const address = useAddressStore((s) => s.address)
 
   if (!mounted) {
     return <Skeleton text>Cargando datos de orden</Skeleton>
