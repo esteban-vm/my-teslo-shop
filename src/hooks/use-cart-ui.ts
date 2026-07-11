8bf0b33c88fc566367e2402be4271ad85dbd611a
Esteban V.M. <estebanvm90@outlook.com>
2026-07-11 09:08:57 -0300

    cleanup
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/hooks/use-cart-ui.ts
+++
@@ -1,5 +0,0 @@
-import type { CartUIStore } from '@/stores'
-import { create } from 'zustand'
-import { createCartUIStore } from '@/stores'
-
-export const useCartUI = create<CartUIStore>()(createCartUIStore)
