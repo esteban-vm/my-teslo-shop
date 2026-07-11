cfa201dab1f0633340a6aa759a532046a5795e76
Esteban V.M. <estebanvm90@outlook.com>
2026-07-11 08:48:40 -0300

    create usePlaceOrder hook
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/hooks/use-place-order.ts
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/navigation'
+import { useAction } from 'next-safe-action/hooks'
+import { placeOrder } from '@/actions/order'
+import { Toasts } from '@/lib/toasts'
+import { useAddressStore } from './use-address-store'
+import { useShoppingCart } from './use-shopping-cart'
+
+export function usePlaceOrder() {
+  const router = useRouter()
+  const resetCart = useShoppingCart((s) => s.resetCart)
+  const resetAddress = useAddressStore((s) => s.resetAddress)
+
+  const hookReturn = useAction(placeOrder, {
+    onSettled() {
+      hookReturn.reset()
+    },
+    onExecute() {
+      Toasts.execute('Colocando orden')
+    },
+    onSuccess(args) {
+      resetCart()
+      resetAddress()
+      Toasts.close()
+      router.replace(`/orders/${args.data.orderId}`)
+    },
+    onError(args) {
+      const { serverError } = args.error
+      if (serverError) Toasts.error(serverError)
+    },
+  })
+
+  return hookReturn
+}
