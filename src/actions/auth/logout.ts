92014d00017853b04533533f4d212fcf16c383cd
Esteban V.M. <estebanvm90@outlook.com>
2026-07-12 18:41:37 -0300

    create logout action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/actions/auth/logout.ts
@@ -0,0 +1,13 @@
+'use server'
+
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { auth } from '@/auth'
+import { sleepExecution } from '@/lib/helpers'
+import { safeClient } from '@/lib/safe-action'
+
+export const logout = safeClient.action(async () => {
+  await sleepExecution(2)
+  await auth.api.signOut({ headers: await headers() })
+  redirect('/')
+})
