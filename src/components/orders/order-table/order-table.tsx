2255faa276fe75d74a3071813f228da9f27fa1fa
Esteban V.M. <estebanvm90@outlook.com>
2026-07-12 16:11:47 -0300

    create order table
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/orders/order-table/order-table.tsx
@@ -0,0 +1,25 @@
+import type { Order } from '@/types'
+import { Table } from 'rsc-daisyui'
+import { OrderRow } from './order-table-row'
+
+export function OrderTable({ orders }: { orders: Order[] }) {
+  return (
+    <section className='mb-3 w-full overflow-x-auto'>
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre completo</th>
+            <th>Estado</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <OrderRow key={order.id} order={order} />
+          ))}
+        </tbody>
+      </Table>
+    </section>
+  )
+}
