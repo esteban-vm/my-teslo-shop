3af50135cd53367672afbf3e6d2e03b42b153b36
Esteban V.M. <estebanvm90@outlook.com>
2026-07-12 18:40:29 -0300

    create manageAddress action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/actions/address/manage-address.ts
@@ -0,0 +1,27 @@
+'use server'
+
+import { redirect } from 'next/navigation'
+import { sleepExecution } from '@/lib/helpers'
+import { prisma } from '@/lib/prisma'
+import { safeAuthClient } from '@/lib/safe-action'
+import { AddressDTO } from '@/schemas/address'
+
+export const manageAddress = safeAuthClient.inputSchema(AddressDTO).action(async ({ ctx, parsedInput }) => {
+  await sleepExecution(3)
+
+  const userId = ctx.user.id
+  const { remember, ...rest } = parsedInput
+
+  if (remember) {
+    const data = { userId, ...rest }
+    await prisma.billingAddress.upsert({ where: { userId }, create: data, update: data })
+  } else {
+    const address = await prisma.billingAddress.findUnique({ where: { userId } })
+
+    if (address) {
+      await prisma.billingAddress.delete({ where: { id: address.id } })
+    }
+  }
+
+  redirect('/checkout')
+})
