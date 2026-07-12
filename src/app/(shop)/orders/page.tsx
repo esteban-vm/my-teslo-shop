b52d38aadf3d329c295155ba415e2ab78c717e5b
Esteban V.M. <estebanvm90@outlook.com>
2026-07-12 16:14:05 -0300

    create admin orders page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/orders/page.tsx
+++ src/app/(shop)/orders/page.tsx
@@ -1,58 +1,21 @@
 export const revalidate = 0
 
 import type { Metadata } from 'next'
-import { Banknote, BanknoteX } from 'lucide-react'
-import NextLink from 'next/link'
-import { Link, Table } from 'rsc-daisyui'
 import { getMyOrders } from '@/actions/order'
+import { OrderTable } from '@/components/orders'
 import { PageTitle } from '@/components/shared'
-import { cn } from '@/lib/ui'
 
 export const metadata: Metadata = {
   title: 'Mis órdenes',
 }
 
-export default async function OrdersPage() {
+export default async function MyOrdersPage() {
   const { data: orders = [] } = await getMyOrders()
 
   return (
     <>
       <PageTitle title='Mis órdenes' />
-      <section className='mb-3 w-full overflow-x-auto'>
-        <Table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Nombre completo</th>
-              <th>Estado</th>
-              <th>Opciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map(({ id, isPaid, shippingAddress }) => {
-              return (
-                <tr className='text-nowrap hover:bg-base-300' key={id}>
-                  <td className='font-semibold'>{id}</td>
-                  <td>
-                    {shippingAddress?.firstName} {shippingAddress?.lastName}
-                  </td>
-                  <td className={cn('[&_svg]:inline [&_svg]:fill-current/30', isPaid ? 'text-success' : 'text-error')}>
-                    {isPaid ? <Banknote /> : <BanknoteX />}&nbsp;
-                    {isPaid ? 'Pagada' : 'No pagada'}
-                  </td>
-                  <td>
-                    <NextLink href={`/orders/${id}`} passHref>
-                      <Link as='span' color='info' hover>
-                        Ver orden
-                      </Link>
-                    </NextLink>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </Table>
-      </section>
+      <OrderTable orders={orders} />
     </>
   )
 }
