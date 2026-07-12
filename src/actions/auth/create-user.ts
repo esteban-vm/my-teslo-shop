9057e8390039bb793f198b9d51ad3ad7b63a7830
Esteban V.M. <estebanvm90@outlook.com>
2026-07-12 18:40:55 -0300

    create createUser action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/actions/auth/create-user.ts
@@ -0,0 +1,13 @@
+'use server'
+
+import { auth } from '@/auth'
+import { sleepExecution } from '@/lib/helpers'
+import { safeClient } from '@/lib/safe-action'
+import { NewUserDTO } from '@/schemas/auth'
+
+export const createUser = safeClient.inputSchema(NewUserDTO).action(async ({ parsedInput }) => {
+  await sleepExecution(3)
+  const { email, name, password } = parsedInput
+  const { user } = await auth.api.signUpEmail({ body: { email, name, password } })
+  return { message: `Correo de verificación enviado a ${user.email}. Por favor, revísalo.` }
+})
