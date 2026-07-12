93d90c46711473a2f8e314e9880a785c767d4405
Esteban V.M. <estebanvm90@outlook.com>
2026-07-12 16:45:36 -0300

    create WithSlug schema
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/schemas/shared.ts
+++ src/schemas/shared.ts
@@ -3,6 +3,8 @@ import { ValidationErrorMap } from '@/lib/constants'
 import { Validations } from '@/lib/validations'
 
 export const WithID = z.object({ id: z.string() })
+export const WithSlug = z.object({ slug: z.string() })
+
 export const notEmpty = z.string().trim().refine(Validations.notEmpty, ValidationErrorMap.notEmpty)
 
 export const passwordParams = {
