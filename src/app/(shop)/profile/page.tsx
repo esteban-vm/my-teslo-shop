8fb9070524d8e6c66fcf0089af798a920aa365cc
Esteban V.M. <estebanvm90@outlook.com>
2026-07-13 09:07:02 -0300

    update profile page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -1,21 +1,18 @@
 import type { Metadata } from 'next'
-import { headers } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { auth } from '@/auth'
 import { PageTitle } from '@/components/shared'
+import { getSession } from '@/lib/auth'
 
 export const metadata: Metadata = {
   title: 'Mi perfil',
 }
 
 export default async function ProfilePage() {
-  const session = await auth.api.getSession({ headers: await headers() })
-  if (!session?.user) redirect('/auth/login')
+  const session = await getSession()
 
   return (
     <>
       <PageTitle title='Mi perfil' />
-      <pre>{JSON.stringify(session.user, null, 2)}</pre>
+      <pre>{JSON.stringify(session?.user, null, 2)}</pre>
     </>
   )
 }
