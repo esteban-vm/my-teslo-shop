8db97e64fabd211c1cffb45e95e9e568468b6688
Esteban V.M. <estebanvm90@outlook.com>
2026-07-13 12:59:14 -0300

    update ChangeUserRole schema
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { Role } from '@/prisma/generated/client'
 
 export const ChangeUserRole = z.object({
-  role: z.enum(Role),
+  role: z.enum(Role).nullable(),
   userId: z.string(),
 })
 
