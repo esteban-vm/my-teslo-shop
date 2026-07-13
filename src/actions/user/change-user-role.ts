fb9a6701bf6cadb51088cb994b83f5d9a3278b07
Esteban V.M. <estebanvm90@outlook.com>
2026-07-13 13:23:19 -0300

    update change suer role action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/user/change-user-role.ts
+++ src/actions/user/change-user-role.ts
@@ -5,12 +5,14 @@ import { revalidatePath } from 'next/cache'
 import { sleepExecution } from '@/lib/helpers'
 import { prisma } from '@/lib/prisma'
 import { safeAdminClient } from '@/lib/safe-action'
-import { ChangeUserRole } from '@/schemas/user'
+import { UserRole } from '@/schemas/user'
 
-export const changeUserRole = safeAdminClient.inputSchema(ChangeUserRole).action(async ({ ctx, parsedInput }) => {
+export const changeUserRole = safeAdminClient.inputSchema(UserRole).action(async ({ ctx, parsedInput }) => {
   await sleepExecution(3)
+
   const { userId, role } = parsedInput
   if (userId === ctx.user.id) return
+
   await prisma.user.update({ where: { id: userId }, data: { role } })
   revalidatePath('/admin/users' satisfies Route)
 })
