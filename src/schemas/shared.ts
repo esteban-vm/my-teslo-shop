addf35575f035128baa7371b1aa7d8a2f779be0e
Esteban V.M. <estebanvm90@outlook.com>
2026-07-13 16:47:14 -0300

    update shared schemas
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/schemas/shared.ts
+++ src/schemas/shared.ts
@@ -5,6 +5,11 @@ import { Validations } from '@/lib/validations'
 export const WithID = z.object({ id: z.string() })
 export const WithSlug = z.object({ slug: z.string() })
 
+export const WithPagination = z.object({
+  page: z.number().optional(),
+  take: z.number().optional(),
+})
+
 export const notEmpty = z.string().trim().refine(Validations.notEmpty, ValidationErrorMap.notEmpty)
 
 export const passwordParams = {
