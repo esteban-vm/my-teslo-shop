88368da33c3c86ae97fb1adf558265bda78ef54b
Esteban V.M. <estebanvm90@outlook.com>
2026-07-13 09:06:09 -0300

    update admin layout
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/admin/layout.tsx
+++ src/app/(shop)/admin/layout.tsx
@@ -1,13 +1,9 @@
-import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { auth } from '@/auth'
+import { getSession } from '@/lib/auth'
 
 export default async function AdminLayout({ children }: LayoutProps<'/admin'>) {
-  const session = await auth.api.getSession({ headers: await headers() })
-
-  if (session?.user.role === 'user') {
-    redirect('/')
-  }
+  const session = await getSession()
+  if (session?.user.role === 'user') redirect('/')
 
   return <>{children}</>
 }
