14a830483596b474dfdfe8d231c785fb024a43d5
Esteban V.M. <estebanvm90@outlook.com>
2026-07-13 13:23:41 -0300

    update role select
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/admin/user-table/role-select.tsx
+++ src/components/admin/user-table/role-select.tsx
@@ -1,21 +1,21 @@
 'use client'
 
-import type { Role } from '@/prisma/generated/client'
-import type { ChangeUserRole } from '@/schemas/user'
+import type { ChangeEventHandler } from 'react'
+import type { UserRole } from '@/schemas/user'
 import { useAction } from 'next-safe-action/hooks'
 import { Select } from 'rsc-daisyui'
 import { changeUserRole } from '@/actions/user'
 import { userRoles } from '@/lib/constants'
 
-export function RoleSelect({ userId, role }: ChangeUserRole) {
+export function RoleSelect({ userId, role }: UserRole) {
   const { execute, isExecuting } = useAction(changeUserRole)
 
+  const onRoleChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    execute({ userId, role: event.target.value as typeof role })
+  }
+
   return (
-    <Select
-      disabled={isExecuting}
-      onChange={(event) => execute({ userId, role: event.target.value as Role })}
-      value={role ?? ''}
-    >
+    <Select disabled={isExecuting} onChange={onRoleChange} value={role ?? ''}>
       {userRoles.map((r) => (
         <option key={r} value={r}>
           {r}
