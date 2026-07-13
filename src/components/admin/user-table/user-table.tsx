a3aef74cd26cd3d89dce5c6d28beaa1058b2f8ad
Esteban V.M. <estebanvm90@outlook.com>
2026-07-13 10:54:02 -0300

    create user table
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/admin/user-table/user-table.tsx
@@ -0,0 +1,25 @@
+import type { UserResult } from '@/types'
+import { Table } from 'rsc-daisyui'
+import { UserRow } from './user-row'
+
+export function UserTable({ users }: { users: UserResult[] }) {
+  return (
+    <section className='mb-3 w-full overflow-x-auto'>
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre completo</th>
+            <th>Correo electrónico</th>
+            <th>Rol</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <UserRow key={user.id} user={user} />
+          ))}
+        </tbody>
+      </Table>
+    </section>
+  )
+}
