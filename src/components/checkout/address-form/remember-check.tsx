7094c8c0245079931b566128a5989d5211933c38
Esteban V.M. <estebanvm90@outlook.com>
2026-07-13 09:50:41 -0300

    create remember check
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/checkout/address-form/remember-check.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import type { FieldValues } from 'react-hook-form'
+import type { FormControlProps } from '@/types'
+import { Controller } from 'react-hook-form'
+import { Checkbox, Fieldset } from 'rsc-daisyui'
+
+export function RememberCheck<T extends FieldValues>({ control, name, ...rest }: FormControlProps<T>) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => {
+        return (
+          <Fieldset.Label className='mt-2 select-none'>
+            <Checkbox {...rest} checked={field.value} size='sm' {...field} />
+            ¿Recordar dirección?
+          </Fieldset.Label>
+        )
+      }}
+    />
+  )
+}
