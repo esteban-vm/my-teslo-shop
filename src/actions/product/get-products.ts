19e333e76969e5a4c3390fd541ac383fd094e10b
Esteban V.M. <estebanvm90@outlook.com>
2026-07-13 16:43:50 -0300

    update get products action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/product/get-products.ts
+++ src/actions/product/get-products.ts
@@ -1,51 +1,39 @@
 'use server'
 
-import type { Gender } from '@/prisma/generated/client'
-import type { ProductResult } from '@/types'
+import type { Paginated, ProductResult } from '@/types'
 import { prisma } from '@/lib/prisma'
+import { safeClient } from '@/lib/safe-action'
+import { Products } from '@/schemas/product'
 
-interface GetProductsParams {
-  page: number
-  take: number
-  gender: Gender
-}
+export const getProducts = safeClient
+  .inputSchema(Products)
+  .action(async ({ parsedInput }): Promise<Paginated<{ products: ProductResult[] }>> => {
+    let { page = 1, take = 12, gender } = parsedInput
+    if (Number.isNaN(page) || page < 1) page = 1
 
-interface GetProductsResult {
-  currentPage: number
-  totalPages: number
-  products: ProductResult[]
-}
-
-export async function getProducts({
-  page = 1,
-  take = 12,
-  gender,
-}: Partial<GetProductsParams>): Promise<GetProductsResult> {
-  if (Number.isNaN(page) || page < 1) page = 1
-
-  const products = await prisma.product.findMany({
-    take,
-    where: { gender },
-    skip: (page - 1) * take,
-    include: {
-      images: {
-        take: 2,
-        select: { url: true },
+    const products = await prisma.product.findMany({
+      take,
+      where: { gender },
+      skip: (page - 1) * take,
+      include: {
+        images: {
+          take: 2,
+          select: { url: true },
+        },
       },
-    },
+    })
+
+    const totalProducts = await prisma.product.count({ where: { gender } })
+    const totalPages = Math.ceil(totalProducts / take)
+
+    return {
+      totalPages,
+      currentPage: page,
+      products: products.map((product) => {
+        return {
+          ...product,
+          images: product.images.map((image) => image.url),
+        }
+      }),
+    }
   })
-
-  const totalProducts = await prisma.product.count({ where: { gender } })
-  const totalPages = Math.ceil(totalProducts / take)
-
-  return {
-    currentPage: page,
-    totalPages,
-    products: products.map((product) => {
-      return {
-        ...product,
-        images: product.images.map((image) => image.url),
-      }
-    }),
-  }
-}
