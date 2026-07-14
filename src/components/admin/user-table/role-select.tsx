6b0c9c4702d0ea474afe60fc8f4c2cb3b690f293
Esteban V.M. <estebanvm90@outlook.com>
2026-07-14 08:40:51 -0300

    update role select
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/admin/user-table/role-select.tsx
+++ src/components/admin/user-table/role-select.tsx
@@ -5,17 +5,21 @@ import type { UserRole } from '@/schemas/user'
 import { useAction } from 'next-safe-action/hooks'
 import { Select } from 'rsc-daisyui'
 import { changeUserRole } from '@/actions/user'
+import { authClient } from '@/auth-client'
 import { userRoles } from '@/lib/constants'
 
 export function RoleSelect({ userId, role }: UserRole) {
+  const { data: session, isPending } = authClient.useSession()
   const { execute, isExecuting } = useAction(changeUserRole)
 
   const onRoleChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
     execute({ userId, role: event.target.value as typeof role })
   }
 
+  const isDisabled = isExecuting || isPending || userId === session?.user.id
+
   return (
-    <Select disabled={isExecuting} onChange={onRoleChange} value={role ?? ''}>
+    <Select disabled={isDisabled} onChange={onRoleChange} value={role ?? ''}>
       {userRoles.map((r) => (
         <option key={r} value={r}>
           {r}
