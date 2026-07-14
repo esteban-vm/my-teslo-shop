698001e39984739ff975da95f83a7602d7e868da
Esteban V.M. <estebanvm90@outlook.com>
2026-07-14 11:45:46 -0300

    create admin layout
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/app/(shop)/admin/layout.ts
@@ -0,0 +1,9 @@
+import { redirect } from 'next/navigation'
+import { getSession } from '@/lib/auth'
+
+export default async function AdminLayout({ children }: LayoutProps<'/admin'>) {
+  const session = await getSession()
+  if (session?.user.role === 'user') redirect('/')
+
+  return children
+}
