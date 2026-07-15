7b4c0191c862441971d3271f8f81bdcb46bb146e
Esteban V.M. <estebanvm90@outlook.com>
2026-07-15 16:23:33 -0300

    update getAllOrders action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/order/get-all-orders.ts
+++ src/actions/order/get-all-orders.ts
@@ -1,34 +1,49 @@
 'use server'
 
-import type { OrderResult } from '@/types'
+import type { OrderResult, Paginated } from '@/types'
 import { prisma } from '@/lib/prisma'
 import { safeAdminClient } from '@/lib/safe-action'
+import { WithPagination } from '@/schemas/shared'
 
-export const getAllOrders = safeAdminClient.action(async (): Promise<OrderResult[]> => {
-  const orders = await prisma.order.findMany({
-    include: {
-      shippingAddress: {
-        select: {
-          firstName: true,
-          lastName: true,
+export const getAllOrders = safeAdminClient
+  .inputSchema(WithPagination)
+  .action(async ({ parsedInput }): Promise<Paginated<{ orders: OrderResult[] }>> => {
+    let { page = 1, take = 12 } = parsedInput
+    if (Number.isNaN(page) || page < 1) page = 1
+
+    const orders = await prisma.order.findMany({
+      take,
+      skip: (page - 1) * take,
+      include: {
+        shippingAddress: {
+          select: {
+            firstName: true,
+            lastName: true,
+          },
         },
       },
-    },
-    omit: {
-      userId: true,
-      total: true,
-      subtotal: true,
-      tax: true,
-      totalItems: true,
-      createdAt: true,
-      updatedAt: true,
-      paidAt: true,
-      transactionId: true,
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-  })
+      omit: {
+        userId: true,
+        total: true,
+        subtotal: true,
+        tax: true,
+        totalItems: true,
+        createdAt: true,
+        updatedAt: true,
+        paidAt: true,
+        transactionId: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
 
-  return orders
-})
+    const totalOrders = await prisma.order.count()
+    const totalPages = Math.ceil(totalOrders / take)
+
+    return {
+      totalPages,
+      currentPage: page,
+      orders,
+    }
+  })
