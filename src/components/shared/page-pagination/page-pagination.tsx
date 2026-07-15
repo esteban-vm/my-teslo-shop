3f2396bacf3d387ac0d87b7e3c8f07715eb5fa4c
Esteban V.M. <estebanvm90@outlook.com>
2026-07-15 10:20:16 -0300

    create page pagination
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/shared/page-pagination/page-pagination.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { Join } from 'rsc-daisyui'
+import { Pagination } from '@/lib/pagination'
+import { PaginationButton } from './pagination-button'
+
+export function PagePagination({ totalPages }: { totalPages: number }) {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const currentPage = Pagination.getCurrentPage({ searchParams })
+  const pageNumbers = Pagination.getPageNumbers({ currentPage, totalPages })
+
+  const getPageUrl = (page: string | number) => {
+    return Pagination.getPageUrl({ page, searchParams, pathname, totalPages })
+  }
+
+  return (
+    <div className='mb-4 text-center'>
+      <Join>
+        <PaginationButton isDisabled={currentPage === 1} to={getPageUrl(currentPage - 1)}>
+          <ChevronLeft />
+        </PaginationButton>
+
+        {pageNumbers.map((page) => {
+          return (
+            <PaginationButton isActive={currentPage === page} key={page} to={getPageUrl(page)}>
+              {page}
+            </PaginationButton>
+          )
+        })}
+
+        <PaginationButton isDisabled={totalPages === currentPage} to={getPageUrl(currentPage + 1)}>
+          <ChevronRight />
+        </PaginationButton>
+      </Join>
+    </div>
+  )
+}
