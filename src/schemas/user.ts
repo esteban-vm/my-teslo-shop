03ab287d18619890b10a5564a311b0e583f5ecbb
Esteban V.M. <estebanvm90@outlook.com>
2026-07-16 15:12:11 -0300

    update user schemas
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,9 +1,22 @@
 import { z } from 'zod'
 import { Role } from '@/prisma/generated/client'
+import { PaginatedResults, WithID } from './shared'
 
-export const UserRole = z.object({
+export const UserResult = WithID.extend({
+  email: z.email(),
+  name: z.string(),
   role: z.enum(Role).nullable(),
-  userId: z.string(),
 })
 
-export type UserRole = z.infer<typeof UserRole>
+export const UserResults = z.array(UserResult)
+export const PaginatedUsers = PaginatedResults.extend({ users: UserResults })
+
+export const UserRoleDTO = z.object({
+  userId: z.string(),
+  role: z.enum(Role).nullable(),
+})
+
+export type UserRoleDTO = z.infer<typeof UserRoleDTO>
+export type UserResult = z.infer<typeof UserResult>
+export type UserResults = z.infer<typeof UserResults>
+export type PaginatedUsers = z.infer<typeof PaginatedUsers>
