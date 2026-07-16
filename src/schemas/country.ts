5051da36542594aa7d2591b552c21ef0b3b5589b
Esteban V.M. <estebanvm90@outlook.com>
2026-07-16 12:39:46 -0300

    update country schemas
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/schemas/country.ts
+++ src/schemas/country.ts
@@ -1,8 +1,8 @@
 import z from 'zod'
 import { WithID } from './shared'
 
-export const CountryDAO = WithID.extend({ name: z.string() })
-export const CountryResults = z.array(CountryDAO)
+export const CountryResult = WithID.extend({ name: z.string() })
+export const CountryResults = z.array(CountryResult)
 
-export type CountryDAO = z.infer<typeof CountryDAO>
+export type CountryResult = z.infer<typeof CountryResult>
 export type CountryResults = z.infer<typeof CountryResults>
