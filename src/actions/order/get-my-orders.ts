8e1c255b74af018f5439f76d92516aa60ac654a0
Esteban V.M. <estebanvm90@outlook.com>
2026-07-16 12:53:01 -0300

    update getMyOrders action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/order/get-my-orders.ts
+++ src/actions/order/get-my-orders.ts
@@ -1,34 +1,53 @@
 'use server'
 
-import type { OrderResult } from '@/types'
 import { prisma } from '@/lib/prisma'
 import { safeAuthClient } from '@/lib/safe-action'
+import { PaginatedOrders } from '@/schemas/order'
+import { WithPagination } from '@/schemas/shared'
 
-export const getMyOrders = safeAuthClient.action(async ({ ctx }): Promise<OrderResult[]> => {
-  const orders = await prisma.order.findMany({
-    where: {
-      userId: ctx.user.id,
-    },
-    include: {
-      shippingAddress: {
-        select: {
-          firstName: true,
-          lastName: true,
+export const getMyOrders = safeAuthClient
+  .inputSchema(WithPagination)
+  .outputSchema(PaginatedOrders)
+  .action(async ({ ctx, parsedInput }) => {
+    let { page = 1, take = 12 } = parsedInput
+    if (Number.isNaN(page) || page < 1) page = 1
+
+    const orders = await prisma.order.findMany({
+      take,
+      skip: (page - 1) * take,
+      where: {
+        userId: ctx.user.id,
+      },
+      include: {
+        shippingAddress: {
+          select: {
+            firstName: true,
+            lastName: true,
+          },
         },
       },
-    },
-    omit: {
-      userId: true,
-      total: true,
-      subtotal: true,
-      tax: true,
-      totalItems: true,
-      createdAt: true,
-      updatedAt: true,
-      paidAt: true,
-      transactionId: true,
-    },
-  })
+      omit: {
+        userId: true,
+        total: true,
+        subtotal: true,
+        tax: true,
+        totalItems: true,
+        createdAt: true,
+        updatedAt: true,
+        paidAt: true,
+        transactionId: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
 
-  return orders
-})
+    const totalOrders = await prisma.order.count()
+    const totalPages = Math.ceil(totalOrders / take)
+
+    return {
+      totalPages,
+      currentPage: page,
+      orders,
+    }
+  })
