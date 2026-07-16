394402c6f4e36c505974213823f8262b5c59ad35
Esteban V.M. <estebanvm90@outlook.com>
2026-07-16 13:27:38 -0300

    update ProductCard
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/product/product-grid/product-card.tsx
+++ src/components/product/product-grid/product-card.tsx
@@ -1,5 +1,5 @@
 import type { Route } from 'next'
-import type { ProductResult } from '@/types'
+import type { ProductResult } from '@/schemas/product'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Card } from 'rsc-daisyui'
