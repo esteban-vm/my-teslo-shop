8539f62761cc1ccc9e2b83847ddfbc861bae2fd0
Esteban V.M. <estebanvm90@outlook.com>
2026-07-16 14:56:43 -0300

    update getStockBySlug action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/product/get-stock-by-slug.ts
+++ src/actions/product/get-stock-by-slug.ts
@@ -1,21 +1,29 @@
 'use server'
 
+import z from 'zod'
 import { sleepExecution } from '@/lib/helpers'
 import { prisma } from '@/lib/prisma'
 import { safeClient } from '@/lib/safe-action'
 import { WithSlug } from '@/schemas/shared'
 
-export const getStockBySlug = safeClient.inputSchema(WithSlug).action(async ({ parsedInput }): Promise<number> => {
-  try {
-    await sleepExecution(3)
+export const getStockBySlug = safeClient
+  .inputSchema(WithSlug)
+  .outputSchema(z.number())
+  .action(async ({ parsedInput }) => {
+    await sleepExecution(2)
 
-    const { stock } = await prisma.product.findFirstOrThrow({
-      where: { slug: parsedInput.slug },
-      select: { stock: true },
-    })
+    try {
+      const { stock } = await prisma.product.findFirstOrThrow({
+        where: {
+          slug: parsedInput.slug,
+        },
+        select: {
+          stock: true,
+        },
+      })
 
-    return stock
-  } catch {
-    return 0
-  }
-})
+      return stock
+    } catch {
+      return 0
+    }
+  })
