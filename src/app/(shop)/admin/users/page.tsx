f5dec4a8dc27cc5b6fa0ab5f7b06793c7e775b7a
Esteban V.M. <estebanvm90@outlook.com>
2026-07-16 15:25:23 -0300

    update UsersPage
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/admin/users/page.tsx
+++ src/app/(shop)/admin/users/page.tsx
@@ -1,19 +1,28 @@
 import type { Metadata } from 'next'
+import { notFound, redirect } from 'next/navigation'
 import { getUsers } from '@/actions/user'
 import { UserTable } from '@/components/admin'
-import { PageTitle } from '@/components/shared'
+import { PagePagination, PageTitle } from '@/components/shared'
 
 const title = 'Mantenimiento de usuarios'
 
 export const metadata: Metadata = { title }
 
-export default async function UsersPage() {
-  const { data: users = [] } = await getUsers()
+export default async function UsersPage({ searchParams }: PageProps<'/admin/users'>) {
+  let { page = '1' } = await searchParams
+  if (Array.isArray(page)) page = '1'
+
+  const { data } = await getUsers({ page: Number.parseInt(page, 10) })
+  if (!data) notFound()
+
+  const { users, totalPages } = data
+  if (users.length === 0) redirect('/admin/users')
 
   return (
     <>
       <PageTitle title={title} />
       <UserTable users={users} />
+      <PagePagination totalPages={totalPages} />
     </>
   )
 }
