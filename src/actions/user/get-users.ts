24e576950fb4a09dda85caa0e40babc37db6cbe6
Esteban V.M. <estebanvm90@outlook.com>
2026-07-16 15:25:02 -0300

    update getUsers action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/user/get-users.ts
+++ src/actions/user/get-users.ts
@@ -1,21 +1,35 @@
 'use server'
 
-import type { UserResult } from '@/types'
 import { prisma } from '@/lib/prisma'
 import { safeAdminClient } from '@/lib/safe-action'
+import { WithPagination } from '@/schemas/shared'
+import { PaginatedUsers } from '@/schemas/user'
 
-export const getUsers = safeAdminClient.action(async (): Promise<UserResult[]> => {
-  const users = await prisma.user.findMany({
-    select: {
-      id: true,
-      email: true,
-      name: true,
-      role: true,
-    },
-    orderBy: {
-      name: 'desc',
-    },
+export const getUsers = safeAdminClient
+  .inputSchema(WithPagination)
+  .outputSchema(PaginatedUsers)
+  .action(async ({ parsedInput }) => {
+    let { page = 1, take = 12 } = parsedInput
+    if (Number.isNaN(page) || page < 1) page = 1
+
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        role: true,
+      },
+      orderBy: {
+        name: 'desc',
+      },
+    })
+
+    const totalUsers = await prisma.user.count()
+    const totalPages = Math.ceil(totalUsers / take)
+
+    return {
+      totalPages,
+      currentPage: page,
+      users,
+    }
   })
-
-  return users
-})
