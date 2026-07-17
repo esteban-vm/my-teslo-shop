64675115ba2184795db6320548e443f08fd17b20
Esteban V.M. <estebanvm90@outlook.com>
2026-07-17 12:35:40 -0300

    create getPageNumber helper
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -20,3 +20,9 @@ export function sleepExecution(seconds = 1) {
 export function isSameProduct(product1: CartProduct, product2: CartProduct) {
   return product1.id === product2.id && product1.size === product2.size
 }
+
+export async function getPageNumber(searchParams: Promise<Record<string, string | string[] | undefined>>) {
+  let { page = '1' } = await searchParams
+  if (Array.isArray(page)) page = '1'
+  return Number.parseInt(page, 10)
+}
