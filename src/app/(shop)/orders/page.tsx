1d3cf220537bcb16e598d454f62796596c36b8d0
Esteban V.M. <estebanvm90@outlook.com>
2026-07-17 12:37:40 -0300

    update OrdersPage
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/orders/page.tsx
+++ src/app/(shop)/orders/page.tsx
@@ -5,16 +5,16 @@ import { notFound, redirect } from 'next/navigation'
 import { getMyOrders } from '@/actions/order'
 import { OrderTable } from '@/components/orders'
 import { PagePagination, PageTitle } from '@/components/shared'
+import { getPageNumber } from '@/lib/helpers'
 
 const title = 'Mis órdenes'
 
 export const metadata: Metadata = { title }
 
 export default async function OrdersPage({ searchParams }: PageProps<'/orders'>) {
-  let { page = '1' } = await searchParams
-  if (Array.isArray(page)) page = '1'
+  const page = await getPageNumber(searchParams)
 
-  const { data } = await getMyOrders({ page: Number.parseInt(page, 10) })
+  const { data } = await getMyOrders({ page })
   if (!data) notFound()
 
   const { orders, totalPages } = data
