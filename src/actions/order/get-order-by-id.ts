d63ba5f84de4d3b3e8894e05d92bf7de02af2f3d
Esteban V.M. <estebanvm90@outlook.com>
2026-07-20 08:40:14 -0300

    update getOrderById action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/order/get-order-by-id.ts
+++ src/actions/order/get-order-by-id.ts
@@ -1,52 +1,53 @@
 'use server'
 
-import { ServerError } from '@/lib/errors'
 import { prisma } from '@/lib/prisma'
 import { safeAuthClient } from '@/lib/safe-action'
+import { OrderByIdResult } from '@/schemas/order'
 import { WithID } from '@/schemas/shared'
 
-export const getOrderById = safeAuthClient.inputSchema(WithID).action(async ({ ctx, parsedInput }) => {
-  const order = await prisma.order.findUnique({
-    where: {
-      id: parsedInput.id,
-      userId: ctx.user.id,
-    },
-    include: {
-      shippingAddress: true,
-      items: {
-        select: {
-          price: true,
-          quantity: true,
-          size: true,
-          product: {
-            select: {
-              id: true,
-              title: true,
-              slug: true,
-              images: {
-                select: {
-                  url: true,
+export const getOrderById = safeAuthClient
+  .inputSchema(WithID)
+  .outputSchema(OrderByIdResult.nullable())
+  .action(async ({ ctx, parsedInput }) => {
+    const order = await prisma.order.findUnique({
+      where: {
+        id: parsedInput.id,
+        userId: ctx.user.id,
+      },
+      include: {
+        shippingAddress: {
+          omit: { id: true, orderId: true },
+        },
+        items: {
+          select: {
+            price: true,
+            quantity: true,
+            size: true,
+            product: {
+              select: {
+                id: true,
+                title: true,
+                slug: true,
+                images: {
+                  select: {
+                    url: true,
+                  },
+                  take: 1,
                 },
-                take: 1,
               },
             },
           },
         },
       },
-    },
-    omit: {
-      id: true,
-      userId: true,
-      createdAt: true,
-      updatedAt: true,
-      paidAt: true,
-      transactionId: true,
-    },
+      omit: {
+        id: true,
+        userId: true,
+        createdAt: true,
+        updatedAt: true,
+        paidAt: true,
+        transactionId: true,
+      },
+    })
+
+    return order
   })
-
-  if (!order) {
-    throw new ServerError('Orden no encontrada')
-  }
-
-  return order
-})
