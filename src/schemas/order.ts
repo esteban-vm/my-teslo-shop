927c9f1d67542f65f133ee5d40837772e0f52221
Esteban V.M. <estebanvm90@outlook.com>
2026-07-20 08:39:46 -0300

    update order schemas
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/schemas/order.ts
+++ src/schemas/order.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod'
 import { Size } from '@/prisma/generated/client'
-import { AddressDTO } from './address'
-import { PaginatedResults, WithID } from './shared'
+import { AddressDTO, AddressResult } from './address'
+import { PaginatedResults } from './shared'
 
-export const OrderResult = WithID.extend({
+export const OrderResult = z.object({
+  id: z.string(),
   isPaid: z.boolean().nullable(),
   shippingAddress: z
     .object({
@@ -18,7 +19,7 @@ export const PaginatedOrders = PaginatedResults.extend({ orders: OrderResults })
 
 export const OrderItemDTO = z.object({
   productId: z.string(),
-  quantity: z.int(),
+  quantity: z.number(),
   size: z.enum(Size),
 })
 
@@ -27,8 +28,37 @@ export const OrderDTO = z.object({
   address: AddressDTO,
 })
 
+export const OrderSummary = z.object({
+  total: z.number(),
+  subtotal: z.number(),
+  tax: z.number(),
+  totalItems: z.number(),
+})
+
+export const OrderByIdResult = z
+  .object({
+    isPaid: z.boolean().nullable(),
+    shippingAddress: AddressResult.nullable(),
+    items: z.array(
+      z.object({
+        quantity: z.number(),
+        size: z.enum(Size),
+        price: z.number(),
+        product: z.object({
+          id: z.string(),
+          title: z.string(),
+          slug: z.string(),
+          images: z.array(z.object({ url: z.string() })),
+        }),
+      })
+    ),
+  })
+  .extend(OrderSummary.shape)
+
 export type OrderResult = z.infer<typeof OrderResult>
 export type OrderResults = z.infer<typeof OrderResults>
 export type PaginatedOrders = z.infer<typeof PaginatedOrders>
 export type OrderItemDTO = z.infer<typeof OrderItemDTO>
 export type OrderDTO = z.infer<typeof OrderDTO>
+export type OrderSummary = z.infer<typeof OrderSummary>
+export type OrderByIdResult = z.infer<typeof OrderByIdResult>
