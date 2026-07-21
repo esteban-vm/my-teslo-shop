91e7d9dc7696abf03b169d0bb7ab83d69a7fcbe4
Esteban V.M. <estebanvm90@outlook.com>
2026-07-21 10:35:57 -0300

    update prisma config
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,6 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@/prisma/generated/client'
 
-const isNotProduction = process.env.NODE_ENV !== 'production'
-
 const pgAdapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
@@ -10,14 +8,14 @@ const pgAdapter = new PrismaPg({
 const prisma = new PrismaClient({
   adapter: pgAdapter,
   errorFormat: 'pretty',
-  log: isNotProduction ? ['query', 'error', 'info', 'warn'] : undefined,
+  log: ['query', 'error'],
 })
 
 const globalForPrisma = global as typeof global & {
   prisma: typeof prisma
 }
 
-if (isNotProduction) {
+if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
 }
 
