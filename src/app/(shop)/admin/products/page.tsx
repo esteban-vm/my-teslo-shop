6645705f65a62d81b4111fb85add26964d6d0c71
Esteban V.M. <estebanvm90@outlook.com>
2026-07-22 16:36:43 -0300

    add new product link
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/admin/products/page.tsx
+++ src/app/(shop)/admin/products/page.tsx
@@ -1,7 +1,9 @@
 export const revalidate = 0
 
 import type { Metadata } from 'next'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
+import { Button } from 'rsc-daisyui'
 import { getProducts } from '@/actions/product'
 import { ProductTable } from '@/components/admin'
 import { PagePagination, PageTitle } from '@/components/shared'
@@ -24,6 +26,9 @@ export default async function Page({ searchParams }: Props) {
   return (
     <>
       <PageTitle title={title} />
+      <Button as={Link} className='float-end' color='info' href='/admin/product/new' outline size='sm'>
+        Nuevo producto
+      </Button>
       <ProductTable products={products} />
       <PagePagination totalPages={totalPages} />
     </>
