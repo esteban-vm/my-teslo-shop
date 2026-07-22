19767b30701cc5b6788a2c523d507f2ddecf879e
Esteban V.M. <estebanvm90@outlook.com>
2026-07-22 11:37:37 -0300

    update product row
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/admin/product-table/product-row.tsx
+++ src/components/admin/product-table/product-row.tsx
@@ -1,17 +1,23 @@
 import type { ProductResult } from '@/schemas/product'
 import Link from 'next/link'
 import { ItemImage } from '@/components/shared'
+import { formatProductPrice } from '@/lib/helpers'
 
 export function ProductRow({ product }: { product: ProductResult }) {
-  const { images, title, price: _, gender: __, stock: ___, sizes: ____, slug } = product
+  const { images, title, price, gender, stock, sizes, slug } = product
 
   return (
     <tr>
       <td>
         <Link href={`/product/${slug}`}>
-          <ItemImage image={images[0]} title={title} />
+          <ItemImage className='size-16' image={images[0]} title={title} />
         </Link>
       </td>
+      <td className='w-full text-left font-semibold'>{title}</td>
+      <td>{formatProductPrice(price)}</td>
+      <td className='uppercase'>{gender}</td>
+      <td>{stock}</td>
+      <td>{sizes.join(', ')}</td>
     </tr>
   )
 }
