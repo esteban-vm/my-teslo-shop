3fc9555f9bfb1332878fa78eb6f0ea623fd94947
Esteban V.M. <estebanvm90@outlook.com>
2026-07-22 08:58:02 -0300

    create product table
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/admin/product-table/product-table.tsx
@@ -0,0 +1,27 @@
+import type { ProductResults } from '@/schemas/product'
+import { Table } from 'rsc-daisyui'
+import { ProductRow } from './product-row'
+
+export function ProductTable({ products }: { products: ProductResults }) {
+  return (
+    <section className='data-table'>
+      <Table>
+        <thead>
+          <tr>
+            <th>Imagen</th>
+            <th>Título</th>
+            <th>Precio</th>
+            <th>Género</th>
+            <th>Inventario</th>
+            <th>Tallas</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <ProductRow key={crypto.randomUUID()} product={product} />
+          ))}
+        </tbody>
+      </Table>
+    </section>
+  )
+}
