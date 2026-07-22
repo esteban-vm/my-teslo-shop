777afb9aa2fb4b0e374198b58387ef01fc7be175
Esteban V.M. <estebanvm90@outlook.com>
2026-07-22 08:57:35 -0300

    update product page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/admin/products/page.tsx
+++ src/app/(shop)/admin/products/page.tsx
@@ -1,17 +1,29 @@
 export const revalidate = 0
 
 import type { Metadata } from 'next'
-import { PageTitle } from '@/components/shared'
+import { notFound } from 'next/navigation'
+import { getProducts } from '@/actions/product'
+import { ProductTable } from '@/components/admin'
+import { PagePagination, PageTitle } from '@/components/shared'
+import { getPageNumber } from '@/lib/helpers'
 
 const title = 'Mantenimiento de productos'
 
 export const metadata: Metadata = { title }
 
-export default function ProductsPage() {
+export default async function ProductsPage({ searchParams }: PageProps<'/admin/products'>) {
+  const page = await getPageNumber(searchParams)
+
+  const { data } = await getProducts({ page, take: 5 })
+  if (!data) notFound()
+
+  const { products, totalPages } = data
+
   return (
     <>
       <PageTitle title={title} />
-      <section></section>
+      <ProductTable products={products} />
+      <PagePagination totalPages={totalPages} />
     </>
   )
 }
