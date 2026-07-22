8aa213ea98cb1df16bf241e3a993daf446ab5d70
Esteban V.M. <estebanvm90@outlook.com>
2026-07-22 11:37:47 -0300

    update item image
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/shared/item-image.tsx
+++ src/components/shared/item-image.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
+import { cn } from '@/lib/ui'
 
 export interface ItemImageProps {
   title: string
   image: string
+  className?: string
 }
 
-export function ItemImage({ title, image }: ItemImageProps) {
+export function ItemImage({ title, image, className }: ItemImageProps) {
   return (
-    <div className='relative size-24 overflow-hidden rounded-box'>
+    <div className={cn('relative size-24 overflow-hidden', className)}>
       <Image alt={title} fill src={`/products/${image}`} />
     </div>
   )
