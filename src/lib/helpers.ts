6f173452c07e901e4d3f6f8cec5cd71eeffae035
Esteban V.M. <estebanvm90@outlook.com>
2026-07-23 08:59:55 -0300

    update helpers
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -22,10 +22,16 @@ export function isSameProduct(product1: CartProduct, product2: CartProduct) {
   return product1.id === product2.id && product1.size === product2.size
 }
 
-export async function getPageNumber(searchParams: Promise<Record<string, string | string[] | undefined>>) {
-  let { page = '1' } = await searchParams
+export async function getSearchParams(searchParams: Promise<Record<string, string | string[] | undefined>>) {
+  let { page = '1', take = '12' } = await searchParams
+
   if (Array.isArray(page)) page = '1'
-  return Number.parseInt(page, 10)
+  if (Array.isArray(take)) take = '12'
+
+  return {
+    page: Number.parseInt(page, 10),
+    take: Number.parseInt(take, 10),
+  }
 }
 
 export function getPagination(input: WithPagination) {
@@ -37,5 +43,8 @@ export function getPagination(input: WithPagination) {
   if (Number.isNaN(pageNumber) || pageNumber < 1) pageNumber = 1
   if (Number.isNaN(takeNumber) || takeNumber < 1) takeNumber = 12
 
-  return { page: pageNumber, take: takeNumber }
+  return {
+    page: pageNumber,
+    take: takeNumber,
+  }
 }
