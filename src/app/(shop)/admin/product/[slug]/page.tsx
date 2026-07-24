438d79b8d52b197c1d318d51737cdb09d9fe9651
Esteban V.M. <estebanvm90@outlook.com>
2026-07-24 15:44:32 -0300

    update product page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/admin/product/[slug]/page.tsx
+++ src/app/(shop)/admin/product/[slug]/page.tsx
@@ -1,7 +1,39 @@
+import type { Metadata } from 'next'
+import { redirect } from 'next/navigation'
+import { getProductBySlug } from '@/actions/product'
+import { ProductForm } from '@/components/admin'
+import { PageTitle } from '@/components/shared'
+
 export type Props = PageProps<'/admin/product/[slug]'>
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params
+  const { data: product } = await getProductBySlug({ slug })
+
+  if (!product) {
+    return {
+      title: 'Producto no encontrado',
+    }
+  }
+
+  const { title, description } = product
+
+  return {
+    title,
+    description,
+  }
+}
 
 export default async function Page({ params }: Props) {
   const { slug } = await params
 
-  return <div>{slug}</div>
+  const { data: product } = await getProductBySlug({ slug })
+  if (!product) redirect('/admin/products')
+
+  return (
+    <>
+      <PageTitle title={product.title} />
+      <ProductForm savedProduct={product} />
+    </>
+  )
 }
