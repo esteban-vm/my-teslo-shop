97e00ee3aaa3bc07ebbfa02cece59683083f120c
Esteban V.M. <estebanvm90@outlook.com>
2026-07-25 12:34:19 -0300

    create signIn action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/auth/index.ts
+++ src/actions/auth/index.ts
@@ -1,3 +1,3 @@
 export * from './create-user'
-export * from './login'
 export * from './logout'
+export * from './sign-in'
--- src/actions/auth/sign-in.ts
+++ src/actions/auth/sign-in.ts
@@ -6,7 +6,7 @@ import { sleepExecution } from '@/lib/helpers'
 import { safeClient } from '@/lib/safe-action'
 import { SignIn } from '@/schemas/auth'
 
-export const login = safeClient.inputSchema(SignIn).action(async ({ parsedInput }) => {
+export const signIn = safeClient.inputSchema(SignIn).action(async ({ parsedInput }) => {
   await sleepExecution(5)
   const { email, password } = parsedInput
   await auth.api.signInEmail({ body: { email, password } })
