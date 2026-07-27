073306336bb657b1482e4aa360c62ce255c0cbcc
Esteban V.M. <estebanvm90@outlook.com>
2026-07-27 15:37:53 -0300

    cleanup
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/admin/product/new/page.tsx
+++
@@ -1,7 +0,0 @@
-export type Props = PageProps<'/admin/product/new'>
-
-export default async function Page({ searchParams }: Props) {
-  const { page = '1' } = await searchParams
-
-  return <div>{page}</div>
-}
