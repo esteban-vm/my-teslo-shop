33e7d8966a5205b66e6de56e5f65b951579b65be
Esteban V.M. <estebanvm90@outlook.com>
2026-07-28 10:26:00 -0300

    create manageAddress action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/actions/product/manage-product.ts
@@ -0,0 +1,25 @@
+'use server'
+
+import type { Product } from '@/prisma/generated/client'
+import type { Size } from '@/prisma/generated/enums'
+import { sleepExecution } from '@/lib/helpers'
+import { prisma } from '@/lib/prisma'
+import { safeAdminClient } from '@/lib/safe-action'
+import { ProductDTO } from '@/schemas/product'
+
+export const manageAddress = safeAdminClient.inputSchema(ProductDTO).action(async ({ parsedInput }) => {
+  await sleepExecution(3)
+
+  await prisma.$transaction(async (_tx) => {
+    const { id, ...rest } = parsedInput
+
+    await prisma.product.upsert({
+      where: { id },
+      create: {} as Product,
+      update: {
+        ...rest,
+        sizes: { set: rest.sizes as Size[] },
+      },
+    })
+  })
+})
