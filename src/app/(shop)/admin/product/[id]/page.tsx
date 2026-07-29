e05bdf32e23cad27424ddc3321159939b5da91f6
Esteban V.M. <estebanvm90@outlook.com>
2026-07-29 09:50:41 -0300

    update product by id page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/admin/product/[id]/page.tsx
+++ src/app/(shop)/admin/product/[id]/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { redirect } from 'next/navigation'
-import { getCategories } from '@/actions/category'
 import { getProductById } from '@/actions/product'
 import { ProductForm } from '@/components/admin'
 import { PageTitle } from '@/components/shared'
@@ -28,14 +27,14 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 export default async function Page({ params }: Props) {
   const { id } = await params
 
-  const [{ data: product }, { data: categories }] = await Promise.all([getProductById({ id }), getCategories()])
+  const { data: product } = await getProductById({ id })
 
   if (!product && !(id === 'new')) redirect('/admin/products')
 
   return (
     <>
       <PageTitle title={id === 'new' ? 'Nuevo producto' : 'Editar producto'} />
-      <ProductForm savedCategories={categories} savedProduct={product} />
+      <ProductForm savedProduct={product} />
     </>
   )
 }
