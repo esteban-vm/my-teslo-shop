ad149ec658475c79eb2542374273a19ac8121914
Esteban V.M. <estebanvm90@outlook.com>
2026-07-29 09:49:16 -0300

    update getCountries action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/country/get-countries.ts
+++ src/actions/country/get-countries.ts
@@ -4,9 +4,8 @@ import { cache } from 'react'
 import { sleepExecution } from '@/lib/helpers'
 import { prisma } from '@/lib/prisma'
 import { safeClient } from '@/lib/safe-action'
-import { CountryResults } from '@/schemas/country'
 
-export const getCountries = safeClient.outputSchema(CountryResults).action(
+export const getCountries = safeClient.action(
   cache(async () => {
     await sleepExecution()
     const countries = await prisma.country.findMany({ orderBy: { name: 'asc' } })
