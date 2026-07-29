3c5695ebfe9d49bdfc5594e73a1cff8f021d3eb0
Esteban V.M. <estebanvm90@outlook.com>
2026-07-29 09:49:31 -0300

    cleanup
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/schemas/category.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-import { CategoryName } from '@/prisma/generated/enums'
-import './config'
-
-export const CategoryResult = z.object({ id: z.string(), name: z.enum(CategoryName) })
-export const CategoryResults = z.array(CategoryResult)
-
-export type CategoryResult = z.infer<typeof CategoryResult>
-export type CategoryResults = z.infer<typeof CategoryResults>
--- src/schemas/country.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-import './config'
-
-export const CountryResult = z.object({ id: z.string(), name: z.string() })
-export const CountryResults = z.array(CountryResult)
-
-export type CountryResult = z.infer<typeof CountryResult>
-export type CountryResults = z.infer<typeof CountryResults>
