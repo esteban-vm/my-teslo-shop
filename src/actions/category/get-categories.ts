45c9adbd2ce45cc03f1b32fb3131094ccf8cc649
Esteban V.M. <estebanvm90@outlook.com>
2026-07-29 09:48:59 -0300

    update getCategories action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/category/get-categories.ts
+++ src/actions/category/get-categories.ts
@@ -4,9 +4,8 @@ import { cache } from 'react'
 import { sleepExecution } from '@/lib/helpers'
 import { prisma } from '@/lib/prisma'
 import { safeClient } from '@/lib/safe-action'
-import { CategoryResults } from '@/schemas/category'
 
-export const getCategories = safeClient.outputSchema(CategoryResults).action(
+export const getCategories = safeClient.action(
   cache(async () => {
     await sleepExecution()
     const categories = await prisma.category.findMany({ orderBy: { name: 'asc' } })
