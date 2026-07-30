9d794572170d9741fcefa4b34cef0e6428618923
Esteban V.M. <estebanvm90@outlook.com>
2026-07-30 15:18:43 -0300

    update resetPassword action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/auth/reset-password.ts
+++ src/actions/auth/reset-password.ts
@@ -1,9 +1,13 @@
 'use server'
 
+import { auth } from '@/auth'
 import { sleepExecution } from '@/lib/helpers'
 import { safeClient } from '@/lib/safe-action'
 import { WithPassword } from '@/schemas/auth'
 
-export const resetPassword = safeClient.inputSchema(WithPassword).action(async () => {
+export const resetPassword = safeClient.inputSchema(WithPassword).action(async ({ parsedInput }) => {
   await sleepExecution(5)
+  const { password, token } = parsedInput
+  await auth.api.resetPassword({ body: { token, newPassword: password } })
+  return { message: 'Contraseña restablecida correctamente' }
 })
