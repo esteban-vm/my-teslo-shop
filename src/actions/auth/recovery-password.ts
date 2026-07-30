45cf4dfbd20a7533661da8649f7702b5ee6205e8
Esteban V.M. <estebanvm90@outlook.com>
2026-07-30 11:53:13 -0300

    update recoveryPassword action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/auth/recovery-password.ts
+++ src/actions/auth/recovery-password.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import type { Route } from 'next'
+import { headers } from 'next/headers'
 import { auth } from '@/auth'
 import { sleepExecution } from '@/lib/helpers'
 import { safeClient } from '@/lib/safe-action'
@@ -9,6 +10,9 @@ import { WithEmail } from '@/schemas/auth'
 export const recoveryPassword = safeClient.inputSchema(WithEmail).action(async ({ parsedInput }) => {
   await sleepExecution(5)
   const { email } = parsedInput
-  await auth.api.requestPasswordReset({ body: { email, redirectTo: '/auth/recovery-password' satisfies Route } })
+  const myHeaders = await headers()
+  const origin = myHeaders.get('origin')
+  const route: Route = '/auth/recovery-password'
+  await auth.api.requestPasswordReset({ body: { email, redirectTo: origin + route } })
   return { message: `Correo de recuperación enviado a ${email}. Por favor, revísalo.` }
 })
