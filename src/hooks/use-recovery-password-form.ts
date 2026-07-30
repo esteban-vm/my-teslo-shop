b2a58f113723a1b29888530ff277d0ff447fbcc8
Esteban V.M. <estebanvm90@outlook.com>
2026-07-30 12:35:28 -0300

    create useRecoveryPasswordForm hook
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/hooks/use-recovery-password-form.ts
@@ -0,0 +1,48 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useHookFormAction } from '@next-safe-action/adapter-react-hook-form/hooks'
+import { useState } from 'react'
+import { recoveryPassword } from '@/actions/auth'
+import { Toasts } from '@/lib/toasts'
+import { WithEmail } from '@/schemas/auth'
+
+export function useRecoveryPasswordForm() {
+  const [isServerError, setIsServerError] = useState(false)
+
+  const hookReturn = useHookFormAction(recoveryPassword, zodResolver(WithEmail), {
+    formProps: {
+      mode: 'onChange',
+      defaultValues: {
+        email: '',
+      },
+    },
+    actionProps: {
+      onSettled() {
+        hookReturn.resetFormAndAction()
+      },
+      onExecute() {
+        Toasts.execute('Enviando correo')
+      },
+      onSuccess(args) {
+        setIsServerError(false)
+        Toasts.success(args.data.message, () => {})
+      },
+      onError(args) {
+        const { serverError } = args.error
+        if (!serverError) return
+        setIsServerError(true)
+        Toasts.error(serverError)
+      },
+      onNavigation() {
+        Toasts.close()
+      },
+    },
+  })
+
+  const { isSubmitting, isSubmitSuccessful } = hookReturn.form.formState
+  const isDisabled = isSubmitting || (!isServerError && isSubmitSuccessful)
+
+  return {
+    ...hookReturn,
+    isDisabled,
+  }
+}
