27fb3616d6cea565a569d9c284fbbe654bf1dfb6
Esteban V.M. <estebanvm90@outlook.com>
2026-07-30 08:31:05 -0300

    update providers
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,29 +1,17 @@
+'use client'
+
 import type { ReactNode } from 'react'
-import { ThemeProvider } from '@teispace/next-themes'
-import { getTheme } from '@teispace/next-themes/server'
-import { ToastContainer } from 'react-toastify'
-import { ThemeMap, themes } from '@/lib/constants'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
-export async function Providers({ children }: { children: ReactNode }) {
-  const initialTheme = await getTheme()
-
+export function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider
-      defaultTheme={themes[1]}
-      disableTransitionOnChange
-      initialTheme={initialTheme ?? undefined}
-      themes={themes}
-      value={ThemeMap}
+    <PayPalScriptProvider
+      options={{
+        environment: 'sandbox',
+        clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
+      }}
     >
-      {/* <PayPalScriptProvider
-        options={{
-          environment: 'sandbox',
-          clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
-        }}
-      > */}
       {children}
-      {/* </PayPalScriptProvider> */}
-      <ToastContainer draggable={false} pauseOnHover={false} position='bottom-center' theme='colored' />
-    </ThemeProvider>
+    </PayPalScriptProvider>
   )
 }
