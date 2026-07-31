f7b9b5728a2956ead4b3afb19fd25d66933bcbea
Esteban V.M. <estebanvm90@outlook.com>
2026-07-31 16:02:01 -0300

    update signUp action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/auth/sign-up.ts
+++ src/actions/auth/sign-up.ts
@@ -1,13 +1,13 @@
 'use server'
 
 import { auth } from '@/auth'
-import { sleep } from '@/lib/helpers'
+import { capitalize, sleep } from '@/lib/helpers'
 import { safeClient } from '@/lib/safe-action'
 import { SignUp } from '@/schemas/auth'
 
 export const signUp = safeClient.inputSchema(SignUp).action(async ({ parsedInput }) => {
   await sleep(3)
   const { email, name, password } = parsedInput
-  const { user } = await auth.api.signUpEmail({ body: { email, name, password } })
+  const { user } = await auth.api.signUpEmail({ body: { email, name: capitalize(name), password } })
   return { message: `Correo de verificación enviado a ${user.email}. Por favor, revísalo.` }
 })
