b00d09dd8916c81ae0f3213dbe6682bd1ec20c4c
Esteban V.M. <estebanvm90@outlook.com>
2026-08-02 11:35:00 -0300

    update product form
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/admin/product-form/product-form.tsx
+++ src/components/admin/product-form/product-form.tsx
@@ -4,6 +4,8 @@ import type { ProductResult } from '@/schemas/product'
 import { Fieldset } from 'rsc-daisyui'
 import { FormField } from '@/components/shared'
 import { useProductForm } from '@/hooks'
+import { CategorySelect } from './category-select'
+import { DescriptionBox } from './description-box'
 
 export interface ProductFormProps {
   savedProduct?: ProductResult | null
@@ -22,6 +24,16 @@ export function ProductForm(props: ProductFormProps) {
         <div className='form-row'>
           <FormField control={control} label='Título' name='title' />
           <FormField control={control} label='Slug' name='slug' spellCheck={false} />
+        </div>
+        <div className='form-row'>
+          <FormField control={control} label='Precio' min={0} name='price' type='number' />
+        </div>
+        <div className='form-row'>
+          <FormField control={control} label='Etiquetas' name='tags' />
+          <CategorySelect control={control} label='Categoría' name='categoryId' />
+        </div>
+        <div className='form-row'>
+          <DescriptionBox control={control} label='Descripción' name='description' />
         </div>
       </Fieldset>
     </form>
