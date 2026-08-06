24d383d122e408f57526aba13e47e8ef65f9f98c
Esteban V.M. <estebanvm90@outlook.com>
2026-08-06 15:08:54 -0300

    create image input
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/admin/product-form/image-input.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useId } from 'react'
+import { FileInput, Label } from 'rsc-daisyui'
+
+export function ImageInput() {
+  const inputId = useId()
+
+  return (
+    <div className='w-full'>
+      <Label as='label' className='required-label' htmlFor={inputId}>
+        Fotos:
+      </Label>
+      <FileInput accept='image/png, image/jpeg' className='w-full' id={inputId} multiple required validator />
+    </div>
+  )
+}
