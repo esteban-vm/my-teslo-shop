69eb66a13a266722a13c578c0ff91527218067d9
Esteban V.M. <estebanvm90@outlook.com>
2026-08-06 15:50:22 -0300

    update home link
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/navigation/navbar/home-link.tsx
+++ src/components/navigation/navbar/home-link.tsx
@@ -1,12 +1,22 @@
-import Link from 'next/link'
+import NextLink from 'next/link'
 import tw from 'tailwind-styled-components'
 
 export function HomeLink() {
   return (
-    <LinkContainer $as={Link} href='/'>
-      <span className='font-bold text-rose-700'>Teslo</span> | Shop
-    </LinkContainer>
+    <>
+      <Link className='motion-safe:hidden' href='/'>
+        <Bold>Teslo</Bold> | Shop
+      </Link>
+
+      <Link className='text-rotate duration-30000 motion-reduce:hidden' href='/'>
+        <span className='justify-items-center'>
+          <Bold>🏪 Teslo</Bold>
+          <span>🛒 Shop</span>
+        </span>
+      </Link>
+    </>
   )
 }
 
-const LinkContainer = tw.a`ml-3 font-montserrat font-semibold hover:opacity-75`
+const Link = tw(NextLink)`ml-3 font-montserrat font-semibold text-lg hover:opacity-75`
+const Bold = tw.span`font-bold text-rose-700`
