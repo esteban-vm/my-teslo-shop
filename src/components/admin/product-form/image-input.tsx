3367f17a9b751ac79e41a232bac50ad57dfe80f6
Esteban V.M. <estebanvm90@outlook.com>
2026-08-11 16:21:43 -0300

    update ImageInput
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/admin/product-form/image-input.tsx
+++ src/components/admin/product-form/image-input.tsx
@@ -1,17 +1,43 @@
 'use client'
 
+import type { FieldValues } from 'react-hook-form'
+import type { NamedFormControlProps } from '@/types'
 import { useId } from 'react'
-import { FileInput, Label } from 'rsc-daisyui'
+import { Controller } from 'react-hook-form'
+import { FileInput, Label, Validator } from 'rsc-daisyui'
+import { ACCEPTED_IMAGE_TYPES } from '@/lib/constants'
 
-export function ImageInput() {
+export function ImageInput<T extends FieldValues>({ control, name }: NamedFormControlProps<T>) {
   const inputId = useId()
+  const errorId = useId()
 
   return (
-    <div className='w-full'>
-      <Label as='label' className='required-label' htmlFor={inputId}>
-        Fotos:
-      </Label>
-      <FileInput accept='image/png, image/jpeg, image/avif' className='w-full' id={inputId} multiple />
-    </div>
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange, value: _, ...rest }, fieldState: { error, invalid } }) => {
+        return (
+          <div className='w-full'>
+            <Label as='label' className='required-label' htmlFor={inputId}>
+              Fotos:
+            </Label>
+            <FileInput
+              accept={ACCEPTED_IMAGE_TYPES.join(', ')}
+              aria-errormessage={errorId}
+              aria-invalid={invalid}
+              className='w-full'
+              id={inputId}
+              multiple
+              onChange={(e) => onChange(e.target.files)}
+              validator
+              {...rest}
+            />
+            <Validator.Hint as='small' id={errorId} role='alert'>
+              {error?.message}
+            </Validator.Hint>
+          </div>
+        )
+      }}
+    />
   )
 }
