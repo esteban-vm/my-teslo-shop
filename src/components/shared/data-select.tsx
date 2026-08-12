2fa089d83df7c92048013d49384e88cdf710a73c
Esteban V.M. <estebanvm90@outlook.com>
2026-08-12 08:10:08 -0300

    update dat select
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/shared/data-select.tsx
+++ src/components/shared/data-select.tsx
@@ -6,7 +6,7 @@ import { useId } from 'react'
 import { Controller } from 'react-hook-form'
 import { Label, Select, Validator } from 'rsc-daisyui'
 
-export type BaseDataSelectProps = Omit<JSX.IntrinsicElements['select'], 'name' | 'size'>
+export type BaseDataSelectProps = Omit<JSX.IntrinsicElements['select'], 'name' | 'size' | 'color'>
 
 export interface DataSelectProps<T extends FieldValues> extends LabeledFormControlProps<T>, BaseDataSelectProps {}
 
@@ -18,7 +18,7 @@ export function DataSelect<T extends FieldValues>({ control, name, label, childr
     <Controller
       control={control}
       name={name}
-      render={({ field, fieldState: { error, isDirty, invalid } }) => {
+      render={({ field, fieldState: { error, invalid } }) => {
         return (
           <div className='w-full'>
             <Label as='label' htmlFor={selectId}>
@@ -29,7 +29,6 @@ export function DataSelect<T extends FieldValues>({ control, name, label, childr
               aria-errormessage={errorId}
               aria-invalid={invalid}
               className='w-full capitalize'
-              color={isDirty && !invalid ? 'success' : undefined}
               id={selectId}
               required
               validator
