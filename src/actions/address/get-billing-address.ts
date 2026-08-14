f79f25a3fb45db14410419f5fc824224a707b6aa
Esteban V.M. <estebanvm90@outlook.com>
2026-08-14 12:04:09 -0300

    update getBillingAddress action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/address/get-billing-address.ts
+++ src/actions/address/get-billing-address.ts
@@ -5,6 +5,6 @@ import { safeAuthClient } from '@/lib/safe-action'
 import { AddressDB } from '@/schemas/address'
 
 export const getBillingAddress = safeAuthClient.outputSchema(AddressDB).action(async ({ ctx }) => {
-  const address = await prisma.billingAddress.findUniqueOrThrow({ where: { userId: ctx.user.id } })
+  const address = await prisma.billingAddress.findUniqueOrThrow({ where: { userId: ctx.auth.user.id } })
   return address
 })
