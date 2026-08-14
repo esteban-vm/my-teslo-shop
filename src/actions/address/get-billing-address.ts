aa0a29028cc28b6804db993edff7b52aeac9718d
Esteban V.M. <estebanvm90@outlook.com>
2026-08-14 10:16:06 -0300

    update getBillingAddress action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/address/get-billing-address.ts
+++ src/actions/address/get-billing-address.ts
@@ -1,19 +1,10 @@
 'use server'
 
-import { cache } from 'react'
-import { ServerError } from '@/lib/errors'
 import { prisma } from '@/lib/prisma'
 import { safeAuthClient } from '@/lib/safe-action'
 import { AddressDB } from '@/schemas/address'
 
-export const getBillingAddress = safeAuthClient.outputSchema(AddressDB).action(
-  cache(async ({ ctx }) => {
-    const address = await prisma.billingAddress.findUnique({ where: { userId: ctx.user.id } })
-
-    if (!address) {
-      throw new ServerError('Dirección no encontrada')
-    }
-
-    return address
-  })
-)
+export const getBillingAddress = safeAuthClient.outputSchema(AddressDB).action(async ({ ctx }) => {
+  const address = await prisma.billingAddress.findUniqueOrThrow({ where: { userId: ctx.user.id } })
+  return address
+})
