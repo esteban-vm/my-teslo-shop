0f729c01ba87f2bb430c094bd11d7d2a3280c9df
Esteban V.M. <estebanvm90@outlook.com>
2026-08-15 08:44:22 -0300

    refactors
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -8,64 +8,55 @@ import { PlaceOrder } from '@/schemas/order'
 
 export const placeOrder = safeAuthClient.inputSchema(PlaceOrder).action(async ({ ctx, parsedInput }) => {
   await sleep(3)
-
   const { items, address } = parsedInput
 
   const products = await prisma.product.findMany({
-    where: {
-      id: {
-        in: items.map((i) => i.productId),
-      },
-    },
+    where: { id: { in: items.map((item) => item.productId) } },
   })
 
-  const totalItems = items.reduce((total, i) => total + i.quantity, 0)
+  const totalItems = items.reduce((total, item) => total + item.quantity, 0)
 
   const { total, subtotal, tax } = items.reduce(
-    (acc, i) => {
-      const product = products.find((p) => p.id === i.productId)
+    (summary, item) => {
+      const product = products.find((product) => product.id === item.productId)
 
       if (!product) {
         throw new ServerError('Producto no encontrado')
       }
 
-      const subtotal = product.price * i.quantity
-      acc.subtotal += subtotal
-      acc.tax += subtotal * 0.15
-      acc.total += subtotal * 1.15
+      const subtotal = product.price * item.quantity
+      summary.subtotal += subtotal
+      summary.tax += subtotal * 0.15
+      summary.total += subtotal * 1.15
 
-      return acc
+      return summary
     },
     { total: 0, subtotal: 0, tax: 0 }
   )
 
   const orderId = await prisma.$transaction(async (tx) => {
-    const productsPromises = products.map((p) => {
-      const quantity = items.filter((i) => i.productId === p.id).reduce((acc, i) => i.quantity + acc, 0)
+    const productsPromises = products.map((product) => {
+      const quantity = items.filter((item) => item.productId === product.id).reduce((q, i) => i.quantity + q, 0)
 
       if (quantity === 0) {
         throw new ServerError('Producto sin existencias')
       }
 
       return tx.product.update({
-        where: { id: p.id },
-        data: {
-          stock: {
-            decrement: quantity,
-          },
-        },
+        where: { id: product.id },
+        data: { stock: { decrement: quantity } },
       })
     })
 
     const updatedProducts = await Promise.all(productsPromises)
 
-    updatedProducts.forEach((p) => {
-      if (p.stock < 0) {
-        throw new ServerError(`${p.title} no tiene inventario suficiente`)
+    updatedProducts.forEach((product) => {
+      if (product.stock < 0) {
+        throw new ServerError(`${product.title} no tiene inventario suficiente`)
       }
     })
 
-    const { id } = await tx.order.create({
+    const { id: orderId } = await tx.order.create({
       data: {
         userId: ctx.auth.user.id,
         tax,
@@ -74,10 +65,10 @@ export const placeOrder = safeAuthClient.inputSchema(PlaceOrder).action(async ({
         totalItems,
         items: {
           createMany: {
-            data: items.map((i) => {
+            data: items.map((item) => {
               return {
-                ...i,
-                price: products.find((p) => p.id === i.productId)?.price ?? 0,
+                ...item,
+                price: products.find((product) => product.id === item.productId)?.price ?? 0,
               }
             }),
           },
@@ -87,8 +78,8 @@ export const placeOrder = safeAuthClient.inputSchema(PlaceOrder).action(async ({
     })
 
     const { remember: _, ...rest } = address
-    await tx.shippingAddress.create({ data: { ...rest, orderId: id } })
-    return id
+    await tx.shippingAddress.create({ data: { ...rest, orderId } })
+    return orderId
   })
 
   return { orderId }
