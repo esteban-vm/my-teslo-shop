9191bfa89c73c70140a350b52dbd4221327e5e34
Esteban V.M. <estebanvm90@outlook.com>
2026-08-16 09:04:08 -0300

    update product image
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/shared/product-image.tsx
+++ src/components/shared/product-image.tsx
@@ -1,12 +1,26 @@
+'use client'
+
 import type { ImageProps } from 'next/image'
 import Image from 'next/image'
+import { useState } from 'react'
 import { getProductImage } from '@/lib/products'
+import { cn } from '@/lib/ui'
 
 export interface ProductImageProps extends Partial<ImageProps> {
   src?: string
 }
 
-export function ProductImage({ src, alt = '', ...rest }: ProductImageProps) {
+export function ProductImage({ src, alt = '', className, ...rest }: ProductImageProps) {
+  const [isLoading, setLoading] = useState(true)
   const imageSrc = getProductImage(src)
-  return <Image alt={alt} src={imageSrc} {...rest} />
+
+  return (
+    <Image
+      alt={alt}
+      className={cn(className, 'transition-[filter] duration-30 ease-in', isLoading ? 'blur-sm' : 'blur-none')}
+      onLoad={() => setLoading(false)}
+      src={imageSrc}
+      {...rest}
+    />
+  )
 }
