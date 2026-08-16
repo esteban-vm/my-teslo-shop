381e96c69ba224656e748e934a0a093aece224b0
Esteban V.M. <estebanvm90@outlook.com>
2026-08-16 08:18:14 -0300

    update product image
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/shared/product-image.tsx
+++ src/components/shared/product-image.tsx
@@ -1,9 +1,12 @@
 import type { ImageProps } from 'next/image'
 import Image from 'next/image'
-import { getProductImage } from '@/lib/helpers'
+import { getProductImage } from '@/lib/products'
 
-export function ProductImage({ src, alt = '', ...rest }: Partial<ImageProps>) {
+export interface ProductImageProps extends Partial<ImageProps> {
+  src?: string
+}
+
+export function ProductImage({ src, alt = '', ...rest }: ProductImageProps) {
   const imageSrc = getProductImage(src)
-
   return <Image alt={alt} src={imageSrc} {...rest} />
 }
