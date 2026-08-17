0b194a097888a405d9137dfce698cc02943e7a72
Esteban V.M. <estebanvm90@outlook.com>
2026-08-17 11:23:39 -0300

    update deleteProductImage action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/product/delete-product-image.ts
+++ src/actions/product/delete-product-image.ts
@@ -1,9 +1,41 @@
 'use server'
 
+import type { Route } from 'next'
+import { revalidatePath } from 'next/cache'
+import { deleteImage } from '@/lib/cloudinary'
+import { DEFAULT_IMAGE_URL } from '@/lib/constants'
+import { sleep } from '@/lib/helpers'
+import { prisma } from '@/lib/prisma'
 import { safeAdminClient } from '@/lib/safe-action'
 import { ProductImage } from '@/schemas/product'
 
 export const deleteProductImage = safeAdminClient.inputSchema(ProductImage).action(async ({ parsedInput }) => {
+  await sleep(3)
+
   const { id, url } = parsedInput
-  console.log({ id, url })
+  if (!url.startsWith('http')) return
+
+  const { id: productId, slug: productSlug } = await prisma.$transaction(async (tx) => {
+    const imageName = url.split('/').pop()?.split('.')[0] ?? ''
+    await deleteImage(imageName)
+
+    const { product } = await tx.picture.delete({
+      where: { id },
+      select: {
+        product: {
+          select: {
+            id: true,
+            slug: true,
+          },
+        },
+      },
+    })
+
+    await tx.picture.create({ data: { url: DEFAULT_IMAGE_URL, productId: product.id } })
+    return product
+  })
+
+  revalidatePath('/admin/products' satisfies Route)
+  revalidatePath(`/admin/product/${productId}` satisfies Route<`/admin/product/${string}`>)
+  revalidatePath(`/product/${productSlug}` satisfies Route<`/product/${string}`>)
 })
