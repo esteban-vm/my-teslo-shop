cd05e857c6d247f668d48f956ccaa79f3fc6cda4
Esteban V.M. <estebanvm90@outlook.com>
2026-08-17 09:03:53 -0300

    add link to edit product
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,12 +1,14 @@
 export const revalidate = 604_800 // 7 días
 
 import type { Metadata } from 'next'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { Suspense } from 'react'
-import { Skeleton } from 'rsc-daisyui'
+import { Button, Skeleton } from 'rsc-daisyui'
 import { getProductBySlug } from '@/actions/product'
 import { QuantitySelector, SizeSelector } from '@/components/cart'
 import { ProductInfo, ProductSlider, StockCounter } from '@/components/product'
+import { getSession } from '@/lib/auth'
 import { formatPrice } from '@/lib/products'
 
 export type Props = PageProps<'/product/[slug]'>
@@ -36,17 +38,27 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function Page({ params }: Props) {
   const { slug } = await params
+  const session = await getSession()
 
   const { data: product } = await getProductBySlug({ slug })
   if (!product) notFound()
 
-  const { title, description, price, sizes, images } = product
+  const { id, title, description, price, sizes, images } = product
 
   return (
     <div className='my-3 grid gap-3 lg:grid-cols-3'>
       <ProductSlider images={images} />
       <div className='space-y-1 px-3 py-1.5'>
         <h1 className='font-bold font-montserrat text-rose-700 text-xl'>{title}</h1>
+
+        {session?.user.role === 'admin' && (
+          <Link href={`/admin/product/${id}`} passHref>
+            <Button as='span' className='my-2' color='info' outline size='sm'>
+              Actualizar producto
+            </Button>
+          </Link>
+        )}
+
         <Suspense fallback={<Skeleton className='h-6 w-full rounded-box' />}>
           <StockCounter slug={slug} />
         </Suspense>
