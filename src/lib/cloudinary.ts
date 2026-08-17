933c7971f300e77b94901b2a0779616e0bfa4c62
Esteban V.M. <estebanvm90@outlook.com>
2026-08-17 15:21:33 -0300

    update uploadImage fn
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -6,6 +6,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-export const uploadImage = (image: string) => cloudinary.uploader.upload(image)
+export const uploadImage = (image: string) => cloudinary.uploader.upload(image, { folder: 'teslo-shop' })
 
 export const deleteImage = (image: string) => cloudinary.uploader.destroy(image)
