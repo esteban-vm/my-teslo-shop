e3da00371e36f5205fe6d18ced34d1c2ec2627c9
Esteban V.M. <estebanvm90@outlook.com>
2026-08-17 11:52:51 -0300

    update image viewer
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/admin/product-form/image-viewer.tsx
+++ src/components/admin/product-form/image-viewer.tsx
@@ -2,11 +2,13 @@
 
 import type { ProductImages } from '@/schemas/product'
 import { CircleXIcon } from 'lucide-react'
+import { useAction } from 'next-safe-action/hooks'
 import { Button, Indicator, Mask } from 'rsc-daisyui'
 import tw from 'tailwind-styled-components'
 import { deleteProductImage } from '@/actions/product'
 import { ProductImage } from '@/components/shared'
 import { DEFAULT_IMAGE_URL } from '@/lib/constants'
+import { Toasts } from '@/lib/toasts'
 import { cn } from '@/lib/ui'
 
 export interface ImageViewerProps {
@@ -14,6 +16,20 @@ export interface ImageViewerProps {
 }
 
 export function ImageViewer({ images }: ImageViewerProps) {
+  const { execute, isExecuting } = useAction(deleteProductImage, {
+    onExecute() {
+      Toasts.execute('Eliminando imagen')
+    },
+    onSuccess() {
+      Toasts.success('Imagen eliminada correctamente')
+    },
+    onError(args) {
+      const { serverError } = args.error
+      if (!serverError) return
+      Toasts.error(serverError)
+    },
+  })
+
   return (
     <Container>
       {images.map((image) => {
@@ -23,8 +39,9 @@ export function ImageViewer({ images }: ImageViewerProps) {
               <Button
                 className={cn('hover:opacity-75', image.url === DEFAULT_IMAGE_URL && 'hidden')}
                 color='error'
+                disabled={isExecuting}
                 link
-                onClick={() => deleteProductImage(image)}
+                onClick={() => execute(image)}
                 shape='circle'
                 size='sm'
                 title='Eliminar imagen'
