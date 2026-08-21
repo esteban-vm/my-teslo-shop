8405a30f936b9fb9563beb9d366ca20a0dfd6a9c
Esteban V.M. <estebanvm90@outlook.com>
2026-08-21 09:31:27 -0300

    update auth layout
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import { redirect } from 'next/navigation'
+import { Hero } from 'rsc-daisyui'
 import tw from 'tailwind-styled-components'
 import { getSession } from '@/lib/auth'
 
@@ -8,7 +9,21 @@ export default async function Layout({ children }: Props) {
   const session = await getSession()
   if (session?.user) redirect('/')
 
-  return <PageContainer>{children}</PageContainer>
+  return (
+    <Hero className='min-h-screen bg-base-200'>
+      <Hero.Content className='flex-col md:flex-row-reverse'>
+        <div className='text-center md:text-left'>
+          <Title>Teslo | Shop</Title>
+          <Paragraph>
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+            deleniti eaque aut repudiandae et a id nisi.
+          </Paragraph>
+        </div>
+        {children}
+      </Hero.Content>
+    </Hero>
+  )
 }
 
-const PageContainer = tw.main`container mx-auto flex min-h-screen items-center justify-center`
+const Title = tw.h1`font-montserrat font-semibold text-4xl text-rose-700 xl:text-5xl`
+const Paragraph = tw.p`text-pretty pt-6 not-xl:pb-6 text-sm xl:text-base`
