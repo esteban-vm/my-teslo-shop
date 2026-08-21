b34ea9803d1a486cc7fc9dd751eaadae766cb2c6
Esteban V.M. <estebanvm90@outlook.com>
2026-08-21 10:08:37 -0300

    update home link style
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/navigation/navbar/home-link.tsx
+++ src/components/navigation/navbar/home-link.tsx
@@ -1,3 +1,4 @@
+import { HandbagIcon, ShirtIcon } from 'lucide-react'
 import NextLink from 'next/link'
 import tw from 'tailwind-styled-components'
 
@@ -5,13 +6,16 @@ export function HomeLink() {
   return (
     <>
       <Link className='motion-safe:hidden' href='/'>
-        <Bold>Teslo</Bold> | Shop
+        <span className='text-rose-700'>Teslo</span> | Shop
       </Link>
-
       <Link className='text-rotate duration-30000 motion-reduce:hidden' href='/'>
         <span className='justify-items-center'>
-          <Bold>🏪 Teslo</Bold>
-          <span>🛒 Shop</span>
+          <span className='text-rose-700'>
+            <Icon $as={ShirtIcon} /> Teslo
+          </span>
+          <span>
+            <Icon $as={HandbagIcon} /> Shop
+          </span>
         </span>
       </Link>
     </>
@@ -19,4 +23,4 @@ export function HomeLink() {
 }
 
 const Link = tw(NextLink)`ml-3 font-montserrat font-semibold text-lg hover:opacity-75`
-const Bold = tw.span`font-bold text-rose-700`
+const Icon = tw.svg`inline fill-current/50 align-text-bottom`
